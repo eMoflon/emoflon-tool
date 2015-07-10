d9ca713bdf3ed13dfa128f83e71fd1454f9dad6b
Roland Kluge <roland.kluge@es.tu-darmstadt.de>
2015-07-10 04:26:15 +0000

    Update post-build event for copying the correct files
    Former-commit-id: 6ebf29abccf55306103faf2c515fa8512c8f19f4

--- org.moflon.ide.eaaddin/eMoflonCommandLine/Properties/AssemblyInfo.cs
+++ org.moflon.ide.eaaddin/eMoflonCommandLine/Properties/AssemblyInfo.cs
@@ -5,13 +5,13 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("eMoflonCommandLine")]
+[assembly: AssemblyTitle("eMoflon::EA command-line tool")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Real-Time Systems Lab, TU Darmstadt")]
 [assembly: AssemblyProduct("eMoflonCommandLine")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("© 2011-2015 Real-Time Systems Lab. Dual license: GNU General Public License version 3 (or above) and Eclipse Public License 1.0 (or above)")]
+[assembly: AssemblyTrademark("eMoflon")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.7.15.0")]
+[assembly: AssemblyFileVersion("1.7.15.0")]
