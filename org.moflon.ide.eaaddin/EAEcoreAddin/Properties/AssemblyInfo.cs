9326802c600929ce69d3a7570a1e2c13c83c9caf
Roland Kluge <roland.kluge@es.tu-darmstadt.de>
2015-07-13 16:16:20 +0000

    Fix another whitespace bug in scripts within the EA project
    Former-commit-id: 1f0f31331164ea3f62d2d18831b3af6f91448f11

--- org.moflon.ide.eaaddin/EAEcoreAddin/Properties/AssemblyInfo.cs
+++ org.moflon.ide.eaaddin/EAEcoreAddin/Properties/AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("http://www.emoflon.org/emoflon/")]
 [assembly: AssemblyProduct("eMoflon::Ecore Extension")]
-[assembly: AssemblyCopyright("© 2011-2015 Real-Time Systems Lab. Dual license: GNU General Public License version 3 (or above) and Eclipse Public License 1.0 (or above)")]
+[assembly: AssemblyCopyright("(c) 2011-2015 Real-Time Systems Lab. Dual license: GNU General Public License version 3 (or above) and Eclipse Public License 1.0 (or above)")]
 [assembly: AssemblyTrademark("eMoflon")]
 [assembly: AssemblyCulture("")]
 
@@ -41,5 +41,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.7.15.0")]
-[assembly: AssemblyFileVersion("1.7.15.0")]
+[assembly: AssemblyVersion("1.7.17.0")]
+[assembly: AssemblyFileVersion("1.7.17.0")]
