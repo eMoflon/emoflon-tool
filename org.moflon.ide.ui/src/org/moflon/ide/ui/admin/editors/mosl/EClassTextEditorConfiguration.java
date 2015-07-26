c654ba06b7a5d79fcc33d0d4007163ff315884eb
Anthony Anjorin <anthony.anjorin@uni-paderborn.de>
2015-07-26 21:40:51 +0000

    This was removing all markers unnecessarily.
    Former-commit-id: 479b89d65ed7adc8e8595c840a7c87a42a2faf45

--- org.moflon.ide.ui/src/org/moflon/ide/ui/admin/editors/mosl/EClassTextEditorConfiguration.java
+++ org.moflon.ide.ui/src/org/moflon/ide/ui/admin/editors/mosl/EClassTextEditorConfiguration.java
@@ -187,7 +187,7 @@ public class EClassTextEditorConfiguration extends AbstractMOSLTextEditorConfigu
    @Override
    public void getProblems()
    {
-      super.getProblems();
+      
    }
 
    @Override
