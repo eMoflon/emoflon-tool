b709b592c783dad6e86d59d04a1dfddf538e782f
Roland Kluge <roland.kluge@es.tu-darmstadt.de>
2015-07-31 07:41:12 +0000

    Refactor paths
    Former-commit-id: 3769624fba78a81011e74a53b8f6a8643e89cbeb

--- org.moflon.tutorial/src/org/moflon/handbook/examples/WizardFinalSolutionTextual.java
+++ org.moflon.tutorial/src/org/moflon/handbook/examples/WizardFinalSolutionTextual.java
@@ -7,28 +7,27 @@ import java.util.List;
 public class WizardFinalSolutionTextual extends AbstractExampleWizard
 {
 
+   private static final String ROOT = "resources/handbook-examples/FinalSolution/Textual";
+
+   private static final String BUNDLE = "org.moflon.CheatSheetTutorial";
+
    @Override
    protected Collection<ProjectDescriptor> getProjectDescriptors()
    {
       List<ProjectDescriptor> projects = new ArrayList<ProjectDescriptor>(1);
-      projects.add(new ProjectDescriptor("org.moflon.CheatSheetTutorial", "resources/handbook-examples/FinalSolution/Textual/Dictionary.zip", "Dictionary"));
+      projects.add(new ProjectDescriptor(BUNDLE, ROOT + "/Dictionary.zip", "Dictionary"));
 
-      projects.add(new ProjectDescriptor("org.moflon.CheatSheetTutorial", "resources/handbook-examples/FinalSolution/Textual/DictionaryCodeAdapter.zip",
-            "DictionaryCodeAdapter"));
+      projects.add(new ProjectDescriptor(BUNDLE, ROOT + "/DictionaryCodeAdapter.zip", "DictionaryCodeAdapter"));
 
-      projects.add(new ProjectDescriptor("org.moflon.CheatSheetTutorial", "resources/handbook-examples/FinalSolution/Textual/DictionaryLanguage.zip",
-            "DictionaryLanguage"));
+      projects.add(new ProjectDescriptor(BUNDLE, ROOT + "/DictionaryLanguage.zip", "DictionaryLanguage"));
 
-      projects.add(new ProjectDescriptor("org.moflon.CheatSheetTutorial", "resources/handbook-examples/FinalSolution/Textual/LearningBoxLanguage.zip",
-            "LearningBoxLanguage"));
+      projects.add(new ProjectDescriptor(BUNDLE, ROOT + "/LearningBoxLanguage.zip", "LearningBoxLanguage"));
 
-      projects.add(new ProjectDescriptor("org.moflon.CheatSheetTutorial",
-            "resources/handbook-examples/FinalSolution/Textual/LearningBoxToDictionaryIntegration.zip", "LearningBoxToDictionaryIntegration"));
+      projects.add(new ProjectDescriptor(BUNDLE, ROOT + "/LearningBoxToDictionaryIntegration.zip", "LearningBoxToDictionaryIntegration"));
 
-      projects.add(new ProjectDescriptor("org.moflon.CheatSheetTutorial", "resources/handbook-examples/FinalSolution/Textual/LeitnersBox.zip", "LeitnersBox"));
+      projects.add(new ProjectDescriptor(BUNDLE, ROOT + "/LeitnersBox.zip", "LeitnersBox"));
 
-      projects.add(new ProjectDescriptor("org.moflon.CheatSheetTutorial", "resources/handbook-examples/FinalSolution/Textual/LeitnersLearningBox.zip",
-            "LeitnersLearningBox"));
+      projects.add(new ProjectDescriptor(BUNDLE, ROOT + "/LeitnersLearningBox.zip", "LeitnersLearningBox"));
 
       return projects;
    }
--- org.moflon.tutorial/src/org/moflon/handbook/examples/WizardFinalSolutionVisual.java
+++ org.moflon.tutorial/src/org/moflon/handbook/examples/WizardFinalSolutionVisual.java
@@ -6,18 +6,18 @@ import java.util.Collection;
 public class WizardFinalSolutionVisual extends AbstractExampleWizard
 {
 
-   private static final String RES_PATH = "resources/handbook-examples/FinalSolution/Visual";
+   private static final String ROOT = "resources/handbook-examples/FinalSolution/Visual";
 
    @Override
    protected Collection<ProjectDescriptor> getProjectDescriptors()
    {
       return Arrays.asList(//
-            new ProjectDescriptor(BUNDLE_ID, RES_PATH + "/Dictionary.zip", "Dictionary"), //
-            new ProjectDescriptor(BUNDLE_ID, RES_PATH + "/DictionaryCodeAdapter.zip", "DictionaryCodeAdapter"), //
-            new ProjectDescriptor(BUNDLE_ID, RES_PATH + "/LearningBoxLanguage.zip", "LearningBoxLanguage"), //
-            new ProjectDescriptor(BUNDLE_ID, RES_PATH + "/LearningBoxToDictionaryIntegration.zip", "LearningBoxToDictionaryIntegration"), //
-            new ProjectDescriptor(BUNDLE_ID, RES_PATH + "/LeitnersBox.zip", "LeitnersBox"), //
-            new ProjectDescriptor(BUNDLE_ID, RES_PATH + "/LeitnersLearningBox.zip", "LeitnersLearningBox"));
+            new ProjectDescriptor(BUNDLE_ID, ROOT + "/Dictionary.zip", "Dictionary"), //
+            new ProjectDescriptor(BUNDLE_ID, ROOT + "/DictionaryCodeAdapter.zip", "DictionaryCodeAdapter"), //
+            new ProjectDescriptor(BUNDLE_ID, ROOT + "/LearningBoxLanguage.zip", "LearningBoxLanguage"), //
+            new ProjectDescriptor(BUNDLE_ID, ROOT + "/LearningBoxToDictionaryIntegration.zip", "LearningBoxToDictionaryIntegration"), //
+            new ProjectDescriptor(BUNDLE_ID, ROOT + "/LeitnersBox.zip", "LeitnersBox"), //
+            new ProjectDescriptor(BUNDLE_ID, ROOT + "/LeitnersLearningBox.zip", "LeitnersLearningBox"));
    }
 
 }
