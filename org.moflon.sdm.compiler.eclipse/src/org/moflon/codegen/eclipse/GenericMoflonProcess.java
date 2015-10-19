c5d264e4b86b42d4fb636a135d9c11651571b7de
Roland Kluge <roland.kluge@es.tu-darmstadt.de>
2015-10-19 16:05:48 +0000

    Docu
    Former-commit-id: 580595f862c0942be69241478dae0aaa52273ffc

--- org.moflon.sdm.compiler.eclipse/src/org/moflon/codegen/eclipse/GenericMoflonProcess.java
+++ org.moflon.sdm.compiler.eclipse/src/org/moflon/codegen/eclipse/GenericMoflonProcess.java
@@ -27,78 +27,114 @@ import org.moflon.properties.MoflonPropertiesContainerHelper;
 
 import MoflonPropertyContainer.MoflonPropertiesContainer;
 
-abstract public class GenericMoflonProcess implements IMonitoredJob {
-	private final IFile ecoreFile;
-	private final ResourceSet resourceSet;
-	private List<Resource> resources;
-	private MoflonPropertiesContainer moflonProperties;
+/**
+ * This class defines a generic process for processing eMoflon projects.
+ * 
+ * @see #run(IProgressMonitor)
+ */
+abstract public class GenericMoflonProcess implements IMonitoredJob
+{
+   private final IFile ecoreFile;
 
-	public GenericMoflonProcess(final IFile ecoreFile, final ResourceSet resourceSet) {
-		this.ecoreFile = ecoreFile;
-		this.resourceSet = resourceSet;
-	}
+   private final ResourceSet resourceSet;
 
-	@Override
-	public final IStatus run(final IProgressMonitor monitor) {
-		try {
-		   monitor.beginTask(getTaskName(), 10);
-			try {
-				// (1) Loads moflon.properties file
-				final IProject project = ecoreFile.getProject();
-				final URI projectURI = URI.createPlatformResourceURI(project.getName() + "/", true);
-				final URI moflonPropertiesURI = URI.createURI(MoflonPropertiesContainerHelper.MOFLON_CONFIG_FILE).resolve(projectURI);
-				final Resource moflonPropertiesResource =
-						CodeGeneratorPlugin.createDefaultResourceSet().getResource(moflonPropertiesURI, true);
-				this.moflonProperties = (MoflonPropertiesContainer) moflonPropertiesResource.getContents().get(0);
+   private List<Resource> resources;
 
-				monitor.worked(1);
-				if (monitor.isCanceled()) {
-					return Status.CANCEL_STATUS;
-				}
-			} catch (WrappedException wrappedException) {
-				final Exception exception = wrappedException.exception();
-				return new Status(IStatus.ERROR, CodeGeneratorPlugin.getModuleID(), exception.getMessage(), exception);
-			} catch (RuntimeException runtimeException) {
-				return new Status(IStatus.ERROR, CodeGeneratorPlugin.getModuleID(), runtimeException.getMessage(), runtimeException);
-			}
+   private MoflonPropertiesContainer moflonProperties;
 
-			// (2) Load metamodel
-			final MonitoredMetamodelLoader metamodelLoader =
-					new MonitoredMetamodelLoader(resourceSet, ecoreFile, moflonProperties);
-			final IStatus metamodelLoaderStatus = metamodelLoader.run(WorkspaceHelper.createSubMonitor(monitor, 2));
-			if (monitor.isCanceled()) {
-				return Status.CANCEL_STATUS;
-			}
-			if (metamodelLoaderStatus.matches(IStatus.ERROR)) {
-				return metamodelLoaderStatus;
-			}
-			this.resources = metamodelLoader.getResources();
+   public GenericMoflonProcess(final IFile ecoreFile, final ResourceSet resourceSet)
+   {
+      this.ecoreFile = ecoreFile;
+      this.resourceSet = resourceSet;
+   }
 
-			return processResource(WorkspaceHelper.createSubMonitor(monitor, 7));
-		} finally {
-			monitor.done();
-		}
-	}
-	
-	abstract public IStatus processResource(final IProgressMonitor monitor);
-	
-	public final IFile getEcoreFile() {
-		return ecoreFile;
-	}
-	
-	public final ResourceSet getResourceSet() {
-		return resourceSet;
-	}
-	
-	public final MoflonPropertiesContainer getMoflonProperties() {
-		return moflonProperties;
-	}
-	
-	public final Resource getEcoreResource() {
-		return resources.get(0);
-	}
-	
-	public final List<Resource> getAllResources() {
-		return resources;
-	}
+   /**
+    * Loads moflon.properties.xmi and the project's meta-model from the specified Ecore file (see constructor).
+    * 
+    * The control flow then continues to {@link GenericMoflonProcess#processResource(IProgressMonitor)}.
+    * 
+    * @see #processResource(IProgressMonitor)
+    */
+   @Override
+   public final IStatus run(final IProgressMonitor monitor)
+   {
+      try
+      {
+         monitor.beginTask(getTaskName(), 10);
+         try
+         {
+            // (1) Loads moflon.properties file
+            final IProject project = ecoreFile.getProject();
+            final URI projectURI = URI.createPlatformResourceURI(project.getName() + "/", true);
+            final URI moflonPropertiesURI = URI.createURI(MoflonPropertiesContainerHelper.MOFLON_CONFIG_FILE).resolve(projectURI);
+            final Resource moflonPropertiesResource = CodeGeneratorPlugin.createDefaultResourceSet().getResource(moflonPropertiesURI, true);
+            this.moflonProperties = (MoflonPropertiesContainer) moflonPropertiesResource.getContents().get(0);
+
+            monitor.worked(1);
+            if (monitor.isCanceled())
+            {
+               return Status.CANCEL_STATUS;
+            }
+         } catch (WrappedException wrappedException)
+         {
+            final Exception exception = wrappedException.exception();
+            return new Status(IStatus.ERROR, CodeGeneratorPlugin.getModuleID(), exception.getMessage(), exception);
+         } catch (RuntimeException runtimeException)
+         {
+            return new Status(IStatus.ERROR, CodeGeneratorPlugin.getModuleID(), runtimeException.getMessage(), runtimeException);
+         }
+
+         // (2) Load metamodel
+         final MonitoredMetamodelLoader metamodelLoader = new MonitoredMetamodelLoader(resourceSet, ecoreFile, moflonProperties);
+         final IStatus metamodelLoaderStatus = metamodelLoader.run(WorkspaceHelper.createSubMonitor(monitor, 2));
+         if (monitor.isCanceled())
+         {
+            return Status.CANCEL_STATUS;
+         }
+         if (metamodelLoaderStatus.matches(IStatus.ERROR))
+         {
+            return metamodelLoaderStatus;
+         }
+         this.resources = metamodelLoader.getResources();
+
+         // Delegate to the subclass
+         return processResource(WorkspaceHelper.createSubMonitor(monitor, 7));
+      } finally
+      {
+         monitor.done();
+      }
+   }
+
+   abstract public IStatus processResource(final IProgressMonitor monitor);
+
+   public final IFile getEcoreFile()
+   {
+      return ecoreFile;
+   }
+
+   public final ResourceSet getResourceSet()
+   {
+      return resourceSet;
+   }
+
+   /**
+    * Returns the stored properties.
+    * 
+    * May be called only after executing {@link #run(IProgressMonitor)} and within
+    * {@link #processResource(IProgressMonitor)}.
+    */
+   public final MoflonPropertiesContainer getMoflonProperties()
+   {
+      return moflonProperties;
+   }
+
+   public final Resource getEcoreResource()
+   {
+      return resources.get(0);
+   }
+
+   public final List<Resource> getAllResources()
+   {
+      return resources;
+   }
 }
