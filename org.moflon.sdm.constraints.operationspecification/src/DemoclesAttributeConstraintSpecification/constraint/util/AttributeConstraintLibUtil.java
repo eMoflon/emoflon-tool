f8ada3f26e528f6908b564e79f45577ee6e61785
Roland Kluge <roland.kluge@es.tu-darmstadt.de>
2015-10-23 09:01:40 +0000

    Add activator for storing the plugin ID
    Former-commit-id: 55f1647b030a430c6a7927397d469b094cf7b333

--- org.moflon.sdm.constraints.operationspecification/src/DemoclesAttributeConstraintSpecification/constraint/util/AttributeConstraintLibUtil.java
+++ org.moflon.sdm.constraints.operationspecification/src/DemoclesAttributeConstraintSpecification/constraint/util/AttributeConstraintLibUtil.java
@@ -7,79 +7,93 @@ import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
+import org.moflno.sdm.constraints.operationspecification.AttributeConstraintsOperationActivator;
 import org.moflon.codegen.eclipse.CodeGeneratorPlugin;
 
 import DemoclesAttributeConstraintSpecification.AttributeConstraintLibrary;
 import DemoclesAttributeConstraintSpecification.DemoclesAttributeConstraintSpecificationFactory;
 import DemoclesAttributeConstraintSpecification.DemoclesAttributeConstraintSpecificationPackage;
 
-
 public class AttributeConstraintLibUtil
 {
-   private static final URI BUILDIN_ATTR_CONST_LIB_URI=URI.createPlatformPluginURI("/DemoclesAttributeConstraintSpecification/lib/buildInConstraintsLibrary/BuildInAttributeVariableConstraintLibrary.xmi",true);
-   
-   
-   
-   public static AttributeConstraintLibrary getBuildInAttributeConstraintLibrary(){
+   public static AttributeConstraintLibrary getBuildInAttributeConstraintLibrary()
+   {
 
-      Resource res=loadAttributeConstraintLibraryResource(BUILDIN_ATTR_CONST_LIB_URI,false);
-      if(res==null){
+      Resource res = loadAttributeConstraintLibraryResource(getUriOfBuiltInLibrary(), false);
+      if (res == null)
+      {
          return null;
       }
       return (AttributeConstraintLibrary) res.getContents().get(0);
-      
-
 
    }
-   public static AttributeConstraintLibrary loadUserDefinedAttributeConstraintLibrary(IProject project){
 
-      String filePath="/"+project.getName()+"/lib/"+project.getName()+"AttributeConstraintsLib.xmi";
-      URI libFileURI=URI.createPlatformResourceURI(filePath, true);
-      Resource res=loadAttributeConstraintLibraryResource(libFileURI, false);
-      if(res==null){
+   public static URI getUriOfBuiltInLibrary()
+   {
+      return URI.createPlatformPluginURI(
+            "/" + AttributeConstraintsOperationActivator.getBundleId() + "/lib/buildInConstraintsLibrary/BuildInAttributeVariableConstraintLibrary.xmi", true);
+   }
+
+   public static AttributeConstraintLibrary loadUserDefinedAttributeConstraintLibrary(final IProject project)
+   {
+
+      String filePath = "/" + project.getName() + "/lib/" + project.getName() + "AttributeConstraintsLib.xmi";
+      URI libFileURI = URI.createPlatformResourceURI(filePath, true);
+      Resource res = loadAttributeConstraintLibraryResource(libFileURI, false);
+      if (res == null)
+      {
          return null;
       }
       return (AttributeConstraintLibrary) res.getContents().get(0);
    }
-   
-   public static AttributeConstraintLibrary getUserDefinedAttributeConstraintLibrary(IProject project){
-      String fileName=project.getName()+"AttributeConstraintsLib";
-      String filePath="/"+project.getName()+"/lib/"+fileName+".xmi";
-      URI libFileURI=URI.createPlatformResourceURI(filePath, true);
-      Resource res=loadAttributeConstraintLibraryResource(libFileURI, true);
-      AttributeConstraintLibrary attributeConstraintLibrary=null;
-      if(res.getContents().size()>0){
-         attributeConstraintLibrary=(AttributeConstraintLibrary) res.getContents().get(0);
+
+   public static AttributeConstraintLibrary getUserDefinedAttributeConstraintLibrary(final IProject project)
+   {
+      String fileName = project.getName() + "AttributeConstraintsLib";
+      String filePath = "/" + project.getName() + "/lib/" + fileName + ".xmi";
+      URI libFileURI = URI.createPlatformResourceURI(filePath, true);
+      Resource res = loadAttributeConstraintLibraryResource(libFileURI, true);
+      AttributeConstraintLibrary attributeConstraintLibrary = null;
+      if (res.getContents().size() > 0)
+      {
+         attributeConstraintLibrary = (AttributeConstraintLibrary) res.getContents().get(0);
       }
-      if(attributeConstraintLibrary==null){
-         attributeConstraintLibrary=DemoclesAttributeConstraintSpecificationFactory.eINSTANCE.createAttributeConstraintLibrary();
+      if (attributeConstraintLibrary == null)
+      {
+         attributeConstraintLibrary = DemoclesAttributeConstraintSpecificationFactory.eINSTANCE.createAttributeConstraintLibrary();
          attributeConstraintLibrary.setPrefix(fileName);
          res.getContents().add(attributeConstraintLibrary);
       }
       return attributeConstraintLibrary;
-      
+
    }
-   private static Resource loadAttributeConstraintLibraryResource(URI uri, boolean createOnDemand){
-      ResourceSet rset= CodeGeneratorPlugin.createDefaultResourceSet();
-      rset.getResourceFactoryRegistry().getExtensionToFactoryMap().put("xmi",new XMIResourceFactoryImpl());
+
+   private static Resource loadAttributeConstraintLibraryResource(final URI uri, final boolean createOnDemand)
+   {
+      ResourceSet rset = CodeGeneratorPlugin.createDefaultResourceSet();
+      rset.getResourceFactoryRegistry().getExtensionToFactoryMap().put("xmi", new XMIResourceFactoryImpl());
       rset.getPackageRegistry().put(DemoclesAttributeConstraintSpecificationPackage.eNS_URI, DemoclesAttributeConstraintSpecificationPackage.eINSTANCE);
-      Resource res=null;
-      if (rset.getURIConverter().exists(uri, null)) {
-         res=rset.getResource(uri, true);
-      }else if(createOnDemand){
-         res=rset.createResource(uri);
+      Resource res = null;
+      if (rset.getURIConverter().exists(uri, null))
+      {
+         res = rset.getResource(uri, true);
+      } else if (createOnDemand)
+      {
+         res = rset.createResource(uri);
       }
       return res;
 
    }
-   public static void saveAttributeConstraintLibrary(AttributeConstraintLibrary attributeConstraintLibrary){
+
+   public static void saveAttributeConstraintLibrary(final AttributeConstraintLibrary attributeConstraintLibrary)
+   {
       try
       {
          attributeConstraintLibrary.eResource().save(null);
       } catch (IOException e)
       {
-           e.printStackTrace();
+         e.printStackTrace();
       }
    }
-   
+
 }
---
+++ org.moflon.sdm.constraints.operationspecification/src/org/moflno/sdm/constraints/operationspecification/AttributeConstraintsOperationActivator.java
@@ -0,0 +1,48 @@
+package org.moflno.sdm.constraints.operationspecification;
+
+import org.eclipse.core.runtime.Plugin;
+import org.osgi.framework.BundleContext;
+
+public class AttributeConstraintsOperationActivator extends Plugin
+{
+   private static AttributeConstraintsOperationActivator plugin;
+
+   private static String bundleId;
+
+   // Singleton instance
+   public static AttributeConstraintsOperationActivator getDefault()
+   {
+      return plugin;
+   }
+
+   public static final String getBundleId()
+   {
+      if (bundleId == null)
+      {
+         throw new NullPointerException();
+      } else
+      {
+         return bundleId;
+      }
+   }
+
+   /**
+    * Executed during plugin startup.
+    * 
+    * @see org.eclipse.core.runtime.Plugin#start(org.osgi.framework.BundleContext)
+    */
+   @Override
+   public void start(final BundleContext context) throws Exception
+   {
+      super.start(context);
+      plugin = this;
+      bundleId = context.getBundle().getSymbolicName();
+   }
+
+   @Override
+   public void stop(final BundleContext context) throws Exception
+   {
+      plugin = null;
+      super.stop(context);
+   }
+}
