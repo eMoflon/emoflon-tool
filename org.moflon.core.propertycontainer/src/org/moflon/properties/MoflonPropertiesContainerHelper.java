a3e22d389599482a32477483427052fa5adfa777
Roland Kluge <roland.kluge@es.tu-darmstadt.de>
2015-10-26 08:22:05 +0000

    Also remove "skipValidation" attribute of moflon.properties.xmi
    Former-commit-id: de99af70f06d1a4ce7b1f96f5478b36e51f3dca5

--- org.moflon.core.propertycontainer/src/org/moflon/properties/MoflonPropertiesContainerHelper.java
+++ org.moflon.core.propertycontainer/src/org/moflon/properties/MoflonPropertiesContainerHelper.java
@@ -45,7 +45,7 @@ public class MoflonPropertiesContainerHelper
 
    // This is the list of XML element tagnames that are no longer supported
    private static final List<String> OBSOLETE_TAGNAMES = Arrays.asList("buildFilter", "core", "debugMode", "genSdmRpCoverageInstrumentation",
-         "genTracingInstrumentation", "injectionErrorHandling", "listShuffling", "strictSDMConditionalBranching");
+         "genTracingInstrumentation", "injectionErrorHandling", "listShuffling", "skipValidation", "strictSDMConditionalBranching");
 
    public static MoflonPropertiesContainer load(final IProject project, final IProgressMonitor monitor)
    {
