e3f95e5a6a218e01c18a52dd6b15bc53e31cd674
Roland Kluge <roland.kluge@es.tu-darmstadt.de>
2015-10-27 09:55:25 +0000

    Add activator to plugin
    Former-commit-id: d95e6dfd9a3079be5991fe67ac03e8e0e086d3ae

---
+++ org.moflon.ide.metamodelevolution.core/src/org/moflon/ide/metamodelevolution/core/MetamodelCoevolutionPlugin.java
@@ -0,0 +1,13 @@
+package org.moflon.ide.metamodelevolution.core;
+
+import org.moflon.core.utilities.EMoflonPlugin;
+
+public class MetamodelCoevolutionPlugin extends EMoflonPlugin
+{
+   public static MetamodelCoevolutionPlugin getDefault() {
+      MetamodelCoevolutionPlugin plugin = getPlugin(MetamodelCoevolutionPlugin.class);
+      if (plugin == null)
+         throw new IllegalStateException("Plugin has not yet been set!");
+      return plugin;
+   }
+}
