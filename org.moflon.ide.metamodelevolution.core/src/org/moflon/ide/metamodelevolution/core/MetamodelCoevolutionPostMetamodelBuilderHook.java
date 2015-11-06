8e97b1f370f68cad917b4726b4dd9a5dbf0d447c
Anne-Sophie Ettl <sophie.ettl@gmail.com>
2015-11-06 12:29:55 +0000

    added project to hook
    Former-commit-id: 54a0363de8e1265caedf3aaf803f2496d7199d5e

--- org.moflon.ide.metamodelevolution.core/src/org/moflon/ide/metamodelevolution/core/MetamodelCoevolutionPostMetamodelBuilderHook.java
+++ org.moflon.ide.metamodelevolution.core/src/org/moflon/ide/metamodelevolution/core/MetamodelCoevolutionPostMetamodelBuilderHook.java
@@ -1,6 +1,7 @@
 package org.moflon.ide.metamodelevolution.core;
 
 import org.apache.log4j.Logger;
+import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.moflon.core.mocatomoflon.Exporter;
@@ -13,10 +14,86 @@ public class MetamodelCoevolutionPostMetamodelBuilderHook implements PostMetamod
    private static final Logger logger = Logger.getLogger(MetamodelBuilder.class);
    
    @Override
-   public IStatus run(final IStatus mocaToMoflonStatus, final MocaTreeEAPropertiesReader mocaTreeReader, final Exporter exporter)
+   public IStatus run(final IStatus mocaToMoflonStatus, final MocaTreeEAPropertiesReader mocaTreeReader, final Exporter exporter, final IProject project)
    {
       logger.debug("Performing post-build step for meta-model co-evolution support");
+      
+      /*IFile mocaFile = WorkspaceHelper.getChangesMocaTree(project);
+
+      if (mocaFile.exists())
+      {
+         // Create and initialize resource set
+         ResourceSet set = CodeGeneratorPlugin.createDefaultResourceSet();
+         eMoflonEMFUtil.installCrossReferencers(set);
+         
+         // Load Moca tree in read-only mode
+         URI mocaFileURI = URI.createPlatformResourceURI(mocaFile.getFullPath().toString(), true);
+         Resource mocaTreeResource = set.getResource(mocaFileURI, true);
+         Node mocaTree = (Node) mocaTreeResource.getContents().get(0);
+         
+         Map<String, String> delta = new HashMap<String, String>();
+         delta = parseChangesTree(mocaTree, delta);
+         System.out.println(delta);
+         processDelta(delta, project);
+      }*/
+      
       return Status.OK_STATUS;
    }
+   
+   /*private Map<String, String> parseChangesTree(final Node tree, Map<String, String> delta)
+   {
+	   if (tree.getName() != null && tree.getName().equals("EClass"))
+	   {
+		   EList<Attribute> attributes2 = tree.getAttribute();
+		   for (Attribute attr : attributes2)
+		   {
+			   if (!(attr.getName().equals("baseClasses")))				   
+				   delta.put(attr.getName(), attr.getValue());
+		   }
+		   return delta;
+	   }	
+	   else 
+	   {
+		   final EList<Text> children = tree.getChildren();
+		   for (Text text : children)
+		   {
+			   Node node = (Node)text;		   			   
+			   parseChangesTree(node, delta);
+		   }
+		   return delta;
+	   }
+	}
+   
+   private void processDelta(Map<String, String> delta, IProject project)
+   {
+	      // adapt java code
+		   RenameRefactoring processor = new RenameClassRefactoring();
+		   processor.refactor(project, delta.get("previousName"), delta.get("name"), delta.get("packageName"));
+		   
+		   RenameRefactoring implProcessor = new RenameClassRefactoring();
+		   implProcessor.refactor(project, "/impl/" + delta.get("previousName") + "Impl", delta.get("name") + "Impl", delta.get("packageName"));
+		   
+		   // process injections
+		   try {
+			   
+			   JavaFileInjectionExtractor extractor = new JavaFileInjectionExtractor();
 
+			   WorkspaceHelper.clearFolder(project, WorkspaceHelper.INJECTION_FOLDER, new NullProgressMonitor());
+			   
+			   IFolder genFolder = WorkspaceHelper.addFolder(project, WorkspaceHelper.GEN_FOLDER, new NullProgressMonitor());
+			   genFolder.accept(new IResourceVisitor()
+				{
+					@Override
+					public boolean visit(IResource resource) throws CoreException {
+						if (resource.getType() == (IResource.FILE))
+							extractor.extractInjectionNonInteractively((IFile) resource);
+						return true;
+					}				
+				}
+				);
+				
+			   } catch (CoreException | URISyntaxException | IOException e) {
+				e.printStackTrace();
+			}
+   }*/
 }
