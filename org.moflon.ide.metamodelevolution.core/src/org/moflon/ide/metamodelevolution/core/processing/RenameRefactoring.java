cbda576a9a6aa5293620b92433a136d56eaaa3bb
Anne-Sophie Ettl <sophie.ettl@gmail.com>
2015-12-03 13:31:39 +0000

    using constants from WorkspaceHelper
    Former-commit-id: c2cc842a516b65e09b09a8361c3a3c720c0b00a6

--- org.moflon.ide.metamodelevolution.core/src/org/moflon/ide/metamodelevolution/core/processing/RenameRefactoring.java
+++ org.moflon.ide.metamodelevolution.core/src/org/moflon/ide/metamodelevolution/core/processing/RenameRefactoring.java
@@ -1,13 +1,14 @@
 package org.moflon.ide.metamodelevolution.core.processing;
 
 import org.eclipse.core.resources.IProject;
+import org.moflon.core.utilities.WorkspaceHelper;
 import org.moflon.ide.metamodelevolution.core.RenameChange;
 
 public interface RenameRefactoring {
 
-	public static final String GEN_FOLDER ="/gen"; //TODO@settl: Use "/" + WorkspaceHelper.GEN_FOLDER
+	public static final String GEN_FOLDER ="/" + WorkspaceHelper.GEN_FOLDER;
 	
-	public static final String JAVA_EXTENSION = ".java"; //TODO@settl: use "." + JAVA_FILE_EXTENSION
+	public static final String JAVA_EXTENSION = "." + WorkspaceHelper.JAVA_FILE_EXTENSION;
 	
 	public void refactor(IProject project, RenameChange change);
 }
