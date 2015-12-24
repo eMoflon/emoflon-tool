7f29e7cf13c3936b804fc27ff95a3faaa18f2c9e
Roland Kluge <roland.kluge@es.tu-darmstadt.de>
2015-12-24 07:58:06 +0000

    Add command for opening dialog for selecting PSF files
    Former-commit-id: d144e3f3379cf77164c6d4a0c197a327c143242e

---
+++ org.moflon.testing.autotester/src/org/moflon/devtools/ui/handlers/install/OpenPsfSelectionDialogHandler.java
@@ -0,0 +1,15 @@
+package org.moflon.devtools.ui.handlers.install;
+
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+public class OpenPsfSelectionDialogHandler extends AbstractInstallCommandHandler
+{
+
+   @Override
+   public Object execute(final ExecutionEvent event) throws ExecutionException
+   {
+      //this.getWorkspaceController(event).installWorkspaceByName(EMoflonStandardWorkspaces.TEST_WORKSPACE_TRANSFORMATION_ZOO_1_NAME);
+      return null;
+   }
+}
