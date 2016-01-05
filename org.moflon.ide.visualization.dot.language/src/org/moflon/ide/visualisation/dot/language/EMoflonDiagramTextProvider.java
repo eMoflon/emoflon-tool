cd567c12b6e66b8d0143722b6f465893f5d4b201
Anthony Anjorin <anthony.anjorin@uni-paderborn.de>
2016-01-05 22:42:09 +0000

    Completely restructured visualization to respond fully incrementally when online changes are made in the open editor.
    Former-commit-id: 42824790979dc0ec566afe9e69f65ef45f439f9b

--- org.moflon.ide.visualization.dot.language/src/org/moflon/ide/visualisation/dot/language/EMoflonDiagramTextProvider.java
+++ org.moflon.ide.visualization.dot.language/src/org/moflon/ide/visualisation/dot/language/EMoflonDiagramTextProvider.java
@@ -6,6 +6,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.log4j.Logger;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.presentation.EcoreEditor;
@@ -14,11 +15,11 @@ import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.ui.IEditorPart;
-import org.eclipse.ui.IPropertyListener;
-import org.eclipse.ui.IWorkbenchPartConstants;
 import org.moflon.core.utilities.MoflonUtilitiesActivator;
 import org.moflon.core.utilities.eMoflonEMFUtil;
 import org.moflon.ide.visualization.dot.language.DirectedGraph;
+import org.moflon.tgg.algorithm.delta.Delta;
+import org.moflon.tgg.algorithm.delta.OnlineChangeDetector;
 import org.moflon.tgg.algorithm.synchronization.SynchronizationHelper;
 import org.moflon.util.eMoflonSDMUtil;
 
@@ -26,12 +27,12 @@ import net.sourceforge.plantuml.eclipse.utils.DiagramTextProvider;
 
 public abstract class EMoflonDiagramTextProvider implements DiagramTextProvider
 {
-   private Map<ISelection, String> cache = new HashMap<>();
-   private IPropertyListener listener = (obj, propId) -> {
-      if (propId == IWorkbenchPartConstants.PROP_DIRTY)
-         cache.clear();
-   };
-	
+   private static final Logger logger = Logger.getLogger(EMoflonDiagramTextProvider.class);
+   private Map<Object, String> diagramTextCache = new HashMap<>();
+   private Map<Object, SynchronizationHelper> incrCache = new HashMap<>();   
+   private Map<Object, Delta> deltaCache = new HashMap<>();
+   private EcoreEditor currentEditor;
+   
    protected abstract String getPluginId();
    protected abstract boolean directionIsForward();
    protected abstract EPackage getPackage();
@@ -51,65 +52,122 @@ public abstract class EMoflonDiagramTextProvider implements DiagramTextProvider
    @Override
    public String getDiagramText(IEditorPart arg0, ISelection selection)
    {
-      if (!cache.containsKey(selection))
+      StructuredSelection structuredSelection = (StructuredSelection) selection;
+      
+      if (null == selection || selection.isEmpty())
+         return "";
+      
+      EObject selectedElement = (EObject) structuredSelection.getFirstElement();
+
+      // Extract input object
+      EObject input = determineInputObjectFromResource(selectedElement.eResource(), selectedElement);
+      if (!diagramTextCache.containsKey(input) || selectionHasBeenChanged(input))
       {
-         StructuredSelection structuredSelection = (StructuredSelection) selection;
-
-         if (null == selection || selection.isEmpty())
-            return "";
-
-         EObject element = (EObject) structuredSelection.getFirstElement();
-
-         String dotDiagram = new DotUnparserAdapter().unparse(modelToDot(element));
-         
-         if(dotDiagram == null)
-            return "";
-         
-         cache.put(selection, dotDiagram);
+         String dotDiagram = new DotUnparserAdapter().unparse(modelToDot(input));
+         if(dotDiagram == null) return "";
+         diagramTextCache.put(input, dotDiagram);
       }
 
-      return cache.get(selection);
+      return diagramTextCache.get(input);
+   }
+   private boolean selectionHasBeenChanged(EObject input)
+   {
+      return deltaCache.containsKey(input) && deltaCache.get(input).isChangeDetected();
    }
 
-   private DirectedGraph modelToDot(final EObject selectedElement)
+   private DirectedGraph modelToDot(final EObject input)
    {
+      final URL pathToPlugin = MoflonUtilitiesActivator.getPathRelToPlugIn("/", getPluginId());   
+      ResourceSet rs = input.eResource().getResourceSet();
+      
+      long tic = System.nanoTime();
       try
       {
-         // Create resource with same URI as loaded resource in editor
-         ResourceSet resourceSet = eMoflonEMFUtil.createDefaultResourceSet();
-         eMoflonEMFUtil.installCrossReferencers(resourceSet);
-         Resource cloneResource = resourceSet.getResource(selectedElement.eResource().getURI(), true);
+         if(incrCache.containsKey(input))
+            return runSync(input);
+         else 
+            return runBatch(pathToPlugin, input, rs);
          
-         // Create helper
-         final URL pathToPlugin = MoflonUtilitiesActivator.getPathRelToPlugIn("/", getPluginId());
-         SynchronizationHelper helper = new SynchronizationHelper(getPackage(), pathToPlugin.getFile(), resourceSet);
-         
-         // Extract selected object
-         EObject input = determineInputObjectFromResource(cloneResource, selectedElement);
-         
-         // Run trafo
-         if(directionIsForward()){
-            helper.setSrc(input);            
-            helper.integrateForward();
-            return (DirectedGraph) helper.getTrg();
-         } else {
-            helper.setTrg(input);            
-            helper.integrateBackward();
-            return (DirectedGraph) helper.getSrc();
-         }
       } catch (final Exception e)
       {
          e.printStackTrace();
+      } finally {
+         long toc = System.nanoTime();
+         logger.info(outputStatistics(input, tic, toc));
+         rs.getResources().removeIf(this::unwantedResource);
       }
 
       return null;
    }
+   private boolean unwantedResource(Resource r)
+   {
+      return r.getURI() != null && r.getURI().toString().endsWith("tempOutputContainer.xmi");
+   }
+   private String outputStatistics(final EObject input, long tic, long toc)
+   {
+      int edges = eMoflonEMFUtil.getEdgeCount(input);
+      int nodes = eMoflonEMFUtil.getNodeCount(input);
+      
+      return "Visualisation of [" + eMoflonEMFUtil.getIdentifier(input) 
+         +  "(E:" + edges + "+ V:" + nodes + " = " + (edges + nodes) + ")] in: " 
+         + (toc - tic) / 1000000000.0 + "s";
+   }
 
+   private DirectedGraph runSync(EObject input)
+   {
+      logger.info("Running synchronization...");
+      SynchronizationHelper helper = incrCache.get(input);
+      helper.setDelta(deltaCache.get(input));
+      DirectedGraph result = runTrafo(helper);
+      deltaCache.get(input).clear();
+      return result;
+   }
+   
+   private DirectedGraph runTrafo(SynchronizationHelper helper)
+   {
+      if(directionIsForward()){
+         helper.integrateForward();
+         return (DirectedGraph) helper.getTrg();
+      } else {
+         helper.integrateBackward();
+         return (DirectedGraph) helper.getSrc();
+      }
+   }
+   
+   private DirectedGraph runBatch(URL pathToPlugin, EObject input, ResourceSet rs)
+   {
+      logger.info("Running batch...");
+
+      SynchronizationHelper helper = new SynchronizationHelper(getPackage(), pathToPlugin.getFile(), rs);
+      helper.setMute(true);
+      incrCache.put(input, helper);
+      
+      if(directionIsForward()) 
+         helper.setSrc(input); 
+      else 
+         helper.setTrg(input);
+            
+      DirectedGraph result = runTrafo(helper);
+      
+      Delta delta = new Delta();
+      new OnlineChangeDetector(delta, input);
+      deltaCache.put(input, delta);
+      
+      return result;
+   }
+   
    @Override
    public boolean supportsEditor(IEditorPart arg0)
    { 
+      if(arg0.equals(currentEditor))
+         return true;
+      
       if(arg0 instanceof EcoreEditor){
-         arg0.addPropertyListener(listener);
+         currentEditor = (EcoreEditor) arg0;
+         diagramTextCache.clear();
+         deltaCache.clear();
+         incrCache.clear();
+         
          return true;
       }
       
