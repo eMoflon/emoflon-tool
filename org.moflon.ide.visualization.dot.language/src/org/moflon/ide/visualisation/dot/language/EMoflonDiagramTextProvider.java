e2c860aadde2a6535095d04d04decbab66c66019
Roland Kluge <roland.kluge@es.tu-darmstadt.de>
2016-01-07 15:32:33 +0000

    Add some docu
    Former-commit-id: 4b9c4058b3c265298a1bef7b51f114df936fa91b

--- org.moflon.ide.visualization.dot.language/src/org/moflon/ide/visualisation/dot/language/EMoflonDiagramTextProvider.java
+++ org.moflon.ide.visualization.dot.language/src/org/moflon/ide/visualisation/dot/language/EMoflonDiagramTextProvider.java
@@ -28,48 +28,107 @@ import net.sourceforge.plantuml.eclipse.utils.DiagramTextProvider;
 public abstract class EMoflonDiagramTextProvider implements DiagramTextProvider
 {
    private static final Logger logger = Logger.getLogger(EMoflonDiagramTextProvider.class);
+
    private Map<Object, String> diagramTextCache = new HashMap<>();
-   private Map<Object, SynchronizationHelper> incrCache = new HashMap<>();   
+
+   private Map<Object, SynchronizationHelper> incrCache = new HashMap<>();
+
    private Map<Object, Delta> deltaCache = new HashMap<>();
+
    private EcoreEditor currentEditor;
-   
+
+   /**
+    * Returns the plugin ID of the plugin that provides the rules for visualizing the supported elements
+    * 
+    * @return
+    */
    protected abstract String getPluginId();
+
+   /**
+    * Returns whether to invoke the TGG in forward (true) or backward (false) direction to obtain an instance of
+    * {@link DirectedGraph}.
+    */
    protected abstract boolean directionIsForward();
+
+   /**
+    * Returns the {@link EPackage} containing the correspondence metamodel for the selected type of transation
+    */
    protected abstract EPackage getPackage();
+
+   /**
+    * Returns whether the given element can be translated by this particular class.
+    */
    protected abstract boolean isElementValidInput(Object selectedElement);
 
-   private EObject determineInputObjectFromResource(Resource resourceForTrafo, EObject selectedElement)
-   {
-      final List<EObject> newSp = new ArrayList<>();
-      resourceForTrafo.getContents().get(0).eAllContents().forEachRemaining(e -> {
-         if(eMoflonSDMUtil.getFQN(e).equals(eMoflonSDMUtil.getFQN(selectedElement)))
-            newSp.add(e);
-      });
-      
-      return newSp.isEmpty()? resourceForTrafo.getContents().get(0) : newSp.get(0);
-   }
-
+   /**
+    * Returns the DOT visualization for the given selection
+    * 
+    * If the selection is null or empty, the result is an empty string.
+    */
    @Override
-   public String getDiagramText(IEditorPart arg0, ISelection selection)
+   public String getDiagramText(IEditorPart editorPart, ISelection selection)
    {
       StructuredSelection structuredSelection = (StructuredSelection) selection;
-      
+   
       if (null == selection || selection.isEmpty())
          return "";
-      
+   
       EObject selectedElement = (EObject) structuredSelection.getFirstElement();
-
+   
       // Extract input object
       EObject input = determineInputObjectFromResource(selectedElement.eResource(), selectedElement);
       if (!diagramTextCache.containsKey(input) || selectionHasBeenChanged(input))
       {
          String dotDiagram = new DotUnparserAdapter().unparse(modelToDot(input));
-         if(dotDiagram == null) return "";
+         if (dotDiagram == null)
+            return "";
          diagramTextCache.put(input, dotDiagram);
       }
-
+   
       return diagramTextCache.get(input);
    }
+
+   @Override
+   public boolean supportsEditor(IEditorPart editorPart)
+   {
+      if (editorPart.equals(currentEditor))
+         return true;
+   
+      if (editorPart instanceof EcoreEditor)
+      {
+         currentEditor = (EcoreEditor) editorPart;
+         diagramTextCache.clear();
+         deltaCache.clear();
+         incrCache.clear();
+   
+         return true;
+      }
+   
+      return false;
+   }
+
+   @Override
+   public boolean supportsSelection(ISelection selection)
+   {
+      StructuredSelection structuredSelection = (StructuredSelection) selection;
+   
+      if (null == selection || selection.isEmpty())
+         return false;
+      else
+         return isElementValidInput(structuredSelection.getFirstElement());
+   }
+
+   private EObject determineInputObjectFromResource(Resource resourceForTrafo, EObject selectedElement)
+   {
+      final List<EObject> newSp = new ArrayList<>();
+      resourceForTrafo.getContents().get(0).eAllContents().forEachRemaining(e -> {
+         if (eMoflonSDMUtil.getFQN(e).equals(eMoflonSDMUtil.getFQN(selectedElement)))
+            newSp.add(e);
+      });
+
+      return newSp.isEmpty() ? resourceForTrafo.getContents().get(0) : newSp.get(0);
+   }
+
    private boolean selectionHasBeenChanged(EObject input)
    {
       return deltaCache.containsKey(input) && deltaCache.get(input).isChangeDetected();
@@ -77,21 +136,22 @@ public abstract class EMoflonDiagramTextProvider implements DiagramTextProvider
 
    private DirectedGraph modelToDot(final EObject input)
    {
-      final URL pathToPlugin = MoflonUtilitiesActivator.getPathRelToPlugIn("/", getPluginId());   
+      final URL pathToPlugin = MoflonUtilitiesActivator.getPathRelToPlugIn("/", getPluginId());
       ResourceSet rs = input.eResource().getResourceSet();
-      
+
       long tic = System.nanoTime();
       try
       {
-         if(incrCache.containsKey(input))
+         if (incrCache.containsKey(input))
             return runSync(input);
-         else 
+         else
             return runBatch(pathToPlugin, input, rs);
-         
+
       } catch (final Exception e)
       {
          e.printStackTrace();
-      } finally {
+      } finally
+      {
          long toc = System.nanoTime();
          logger.info(outputStatistics(input, tic, toc));
          rs.getResources().removeIf(this::unwantedResource);
@@ -99,18 +159,19 @@ public abstract class EMoflonDiagramTextProvider implements DiagramTextProvider
 
       return null;
    }
+
    private boolean unwantedResource(Resource r)
    {
       return r.getURI() != null && r.getURI().toString().endsWith("tempOutputContainer.xmi");
    }
+
    private String outputStatistics(final EObject input, long tic, long toc)
    {
       int edges = eMoflonEMFUtil.getEdgeCount(input);
       int nodes = eMoflonEMFUtil.getNodeCount(input);
-      
-      return "Visualisation of [" + eMoflonEMFUtil.getIdentifier(input) 
-         +  "(E:" + edges + "+ V:" + nodes + " = " + (edges + nodes) + ")] in: " 
-         + (toc - tic) / 1000000000.0 + "s";
+
+      return "Visualisation of [" + eMoflonEMFUtil.getIdentifier(input) + "(E:" + edges + "+ V:" + nodes + " = " + (edges + nodes) + ")] in: "
+            + (toc - tic) / 1000000000.0 + "s";
    }
 
    private DirectedGraph runSync(EObject input)
@@ -122,18 +183,20 @@ public abstract class EMoflonDiagramTextProvider implements DiagramTextProvider
       deltaCache.get(input).clear();
       return result;
    }
-   
+
    private DirectedGraph runTrafo(SynchronizationHelper helper)
    {
-      if(directionIsForward()){
+      if (directionIsForward())
+      {
          helper.integrateForward();
          return (DirectedGraph) helper.getTrg();
-      } else {
+      } else
+      {
          helper.integrateBackward();
          return (DirectedGraph) helper.getSrc();
       }
    }
-   
+
    private DirectedGraph runBatch(URL pathToPlugin, EObject input, ResourceSet rs)
    {
       logger.info("Running batch...");
@@ -141,48 +204,19 @@ public abstract class EMoflonDiagramTextProvider implements DiagramTextProvider
       SynchronizationHelper helper = new SynchronizationHelper(getPackage(), pathToPlugin.getFile(), rs);
       helper.setMute(true);
       incrCache.put(input, helper);
-      
-      if(directionIsForward()) 
-         helper.setSrc(input); 
-      else 
+
+      if (directionIsForward())
+         helper.setSrc(input);
+      else
          helper.setTrg(input);
-            
+
       DirectedGraph result = runTrafo(helper);
-      
+
       Delta delta = new Delta();
       new OnlineChangeDetector(delta, input);
       deltaCache.put(input, delta);
-      
+
       return result;
-   }
-   
-   @Override
-   public boolean supportsEditor(IEditorPart arg0)
-   { 
-      if(arg0.equals(currentEditor))
-         return true;
-      
-      if(arg0 instanceof EcoreEditor){
-         currentEditor = (EcoreEditor) arg0;
-         diagramTextCache.clear();
-         deltaCache.clear();
-         incrCache.clear();
-         
-         return true;
-      }
-      
-      return false;
-   }
-
-   @Override
-   public boolean supportsSelection(ISelection selection)
-   {
-      StructuredSelection structuredSelection = (StructuredSelection) selection;
-
-      if (null == selection || selection.isEmpty())
-         return false;
-      else 
-         return isElementValidInput(structuredSelection.getFirstElement());
    }
 
 }
