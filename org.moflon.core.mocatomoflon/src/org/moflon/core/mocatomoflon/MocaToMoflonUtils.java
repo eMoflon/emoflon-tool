47e0f41672a4b4e1cbd5352841fdbd34a2fdcb1a
Anne-Sophie Ettl <sophie.ettl@gmail.com>
2016-02-23 11:54:03 +0000

    added more metamodelevolution types
    Former-commit-id: 3593332ee9107c186dc557abb371eb63eaa7baff

--- org.moflon.core.mocatomoflon/src/org/moflon/core/mocatomoflon/MocaToMoflonUtils.java
+++ org.moflon.core.mocatomoflon/src/org/moflon/core/mocatomoflon/MocaToMoflonUtils.java
@@ -6,8 +6,19 @@ public class MocaToMoflonUtils
 {
    public static final String ECLASS_NODE_NAME = "EClass";
    
+   public static final String EPACKAGE_NODE_NAME = "EPackage";
+   
+   public static final String TLPACKAGE_NODE_NAME = "TLPackage";
 
    public static boolean isEClassNode(final Node node) {
       return node.getName() != null && (node.getName().equals(ECLASS_NODE_NAME));
    }
+   
+   public static boolean isEPackageNode(final Node node) {
+	      return node.getName() != null && (node.getName().equals(EPACKAGE_NODE_NAME));
+   }
+   
+   public static boolean isTLPackageNode(final Node node) {
+	      return node.getName() != null && (node.getName().equals(TLPACKAGE_NODE_NAME));
+   }   
 }
