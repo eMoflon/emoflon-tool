5d7ad775db93a301dd5eb2734a66e63763591239
Anne-Sophie Ettl <sophie.ettl@gmail.com>
2016-02-28 20:59:27 +0000

    deleted unused files
    Former-commit-id: f928cc3bf13365b0874ea41b5fd521f3e5ff9863

--- org.moflon.ide.metamodelevolution.core/src/org/moflon/ide/metamodelevolution/core/util/MetamodelComparator.java
+++
@@ -1,9 +0,0 @@
-package org.moflon.ide.metamodelevolution.core.util;
-
-import org.eclipse.emf.ecore.EPackage;
-
-public interface MetamodelComparator
-{
-
-   MetamodelDelta compare(EPackage oldMetaModel, EPackage newMetaModel);
-}
--- org.moflon.ide.metamodelevolution.core/src/org/moflon/ide/metamodelevolution/core/util/MetamodelDelta.java
+++
@@ -1,6 +0,0 @@
-package org.moflon.ide.metamodelevolution.core.util;
-
-public interface MetamodelDelta
-{
-
-}
