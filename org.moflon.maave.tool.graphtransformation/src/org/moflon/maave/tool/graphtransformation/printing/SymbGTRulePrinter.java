7ba4da6eb01532a488da6636fbb4458a0fe76746
Frederick Deckwerth <frederick.deckwerth@es.tu-darmstadt.de>
2016-03-29 16:20:57 +0000

    SymbGTRulePrinter prints nor rule name on top

--- org.moflon.maave.tool.graphtransformation/src/org/moflon/maave/tool/graphtransformation/printing/SymbGTRulePrinter.java
+++ org.moflon.maave.tool.graphtransformation/src/org/moflon/maave/tool/graphtransformation/printing/SymbGTRulePrinter.java
@@ -8,11 +8,12 @@ import org.moflon.maave.tool.symbolicgraphs.printing.GraphAndMorphismPrinter;
 public class SymbGTRulePrinter
 {
    public static String print(SymbGTRule rule){
+      
       SymbolicGraphMorphism l=rule.getLeft();
       SymbolicGraphMorphism r=rule.getRight();
      
       
-      return GraphAndMorphismPrinter.internalPrintSpan(l, r);
+      return rule.getName()+"\n"+GraphAndMorphismPrinter.internalPrintSpan(l, r);
    
       
    }
