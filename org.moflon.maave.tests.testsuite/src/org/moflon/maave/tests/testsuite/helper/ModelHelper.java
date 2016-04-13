df7d3c9b4287ec8092358caceba019a04ff71fb9
Roland Kluge <roland.kluge@es.tu-darmstadt.de>
2016-04-13 10:14:24 +0000

    Add handler for retrieving specific patterns

--- org.moflon.maave.tests.testsuite/src/org/moflon/maave/tests/testsuite/helper/ModelHelper.java
+++ org.moflon.maave.tests.testsuite/src/org/moflon/maave/tests/testsuite/helper/ModelHelper.java
@@ -17,42 +17,52 @@ import org.moflon.maave.tool.symbolicgraphs.secondorder.matching.MatchingUtils.M
 import SDMLanguage.activities.MoflonEOperation;
 import SDMLanguage.activities.StoryNode;
 
-public class ModelHelper
-{
-   private static final Transformer transformer=StptransformationFactory.eINSTANCE.createTransformer();
-   
-   
-   public static SymbGTRule getRule(EClass cls,String name)
-   {
-      MoflonEOperation op1=(MoflonEOperation) cls.getEOperations().stream().filter(x->x.getName().equals(name)).findFirst().get();
-      if(op1==null)
-      {
-         throw new NullPointerException("No operation found with name:"+name+" in class"+cls.getName());
-      }
-      StoryNode stn1=(StoryNode) op1.getActivity().getOwnedActivityNode().stream().filter(x->x instanceof StoryNode).collect(Collectors.toList()).get(0);
-      return transformer.transformStpToProjGTRule(stn1.getStoryPattern());
-   }
+public class ModelHelper {
+	private static final Transformer transformer = StptransformationFactory.eINSTANCE.createTransformer();
 
-   public static NegativeConstraint getUserDefConstraints(EPackage pack)
-   {
-      //UserDefConstraints
-      EClass clsConstr=(EClass) pack.getEClassifier("MetamodelConstraints");
-      List<EOperation> ncOps= clsConstr.getEOperations().stream().filter(x->x.getName().startsWith("_NC_")).collect(Collectors.toList());
+	public static SymbGTRule getRule(EClass cls, String name) {
+		MoflonEOperation op1 = (MoflonEOperation) cls.getEOperations().stream().filter(x -> x.getName().equals(name))
+				.findFirst().get();
+		if (op1 == null) {
+			throw new NullPointerException("No operation found with name:" + name + " in class" + cls.getName());
+		}
+		StoryNode stn1 = (StoryNode) op1.getActivity().getOwnedActivityNode().stream()
+				.filter(x -> x instanceof StoryNode).collect(Collectors.toList()).get(0);
+		return transformer.transformStpToProjGTRule(stn1.getStoryPattern());
+	}
 
+	public static SymbGTRule getPattern(EClass cls, String operationName, String activityNodeName) {
+		MoflonEOperation op1 = (MoflonEOperation) cls.getEOperations().stream()
+				.filter(x -> x.getName().equals(operationName)).findFirst().get();
+		if (op1 == null) {
+			throw new NullPointerException(
+					"No operation found with name:" + operationName + " in class" + cls.getName());
+		}
+		StoryNode stn1 = (StoryNode) op1.getActivity().getOwnedActivityNode().stream()
+				.filter(x -> x instanceof StoryNode).filter(x -> activityNodeName.equals(x.getName()))
+				.collect(Collectors.toList()).get(0);
+		return transformer.transformStpToProjGTRule(stn1.getStoryPattern());
+	}
 
-      NegativeConstraint nC=ConditionsFactory.eINSTANCE.createNegativeConstraint();
-      ConfigurableMorphismClassFactory morClassFac=MatchingUtilsFactory.eINSTANCE.createConfigurableMorphismClassFactory();
-      nC.setMorphismClass(morClassFac.createMorphismClass("I", "I", "I", "I", "=>"));
-      for (EOperation eOperation : ncOps)
-      {
-         MoflonEOperation mEOp=(MoflonEOperation) eOperation;
-         StoryNode constraintStn=(StoryNode) mEOp.getActivity().getOwnedActivityNode().stream().filter(x->x instanceof StoryNode).findAny().get();
-         SymbGTRule ruleC=transformer.transformStpToProjGTRule(constraintStn.getStoryPattern());
-         nC.getAtomicNegativeConstraints().add(ruleC.getLeft().getCodom());
+	public static NegativeConstraint getUserDefConstraints(EPackage pack) {
+		// UserDefConstraints
+		EClass clsConstr = (EClass) pack.getEClassifier("MetamodelConstraints");
+		List<EOperation> ncOps = clsConstr.getEOperations().stream().filter(x -> x.getName().startsWith("_NC_"))
+				.collect(Collectors.toList());
 
-      }
+		NegativeConstraint nC = ConditionsFactory.eINSTANCE.createNegativeConstraint();
+		ConfigurableMorphismClassFactory morClassFac = MatchingUtilsFactory.eINSTANCE
+				.createConfigurableMorphismClassFactory();
+		nC.setMorphismClass(morClassFac.createMorphismClass("I", "I", "I", "I", "=>"));
+		for (EOperation eOperation : ncOps) {
+			MoflonEOperation mEOp = (MoflonEOperation) eOperation;
+			StoryNode constraintStn = (StoryNode) mEOp.getActivity().getOwnedActivityNode().stream()
+					.filter(x -> x instanceof StoryNode).findAny().get();
+			SymbGTRule ruleC = transformer.transformStpToProjGTRule(constraintStn.getStoryPattern());
+			nC.getAtomicNegativeConstraints().add(ruleC.getLeft().getCodom());
 
+		}
 
-      return nC;
-   }
+		return nC;
+	}
 }
