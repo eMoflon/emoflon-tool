dd2fd95c0be6511a8823c00514d3a3395d963f70
Erhan Leblebici <erhan.leblebici@es.tu-darmstadt.de>
2016-05-05 15:57:49 +0000

    cleaned up import mechanism in post processing
    Former-commit-id: 86f79b69d42f05040f8a5839a6ec8b71fcd4b0ee

--- org.moflon.tgg.mosl.codeadapter/src/org/moflon/tie/CodeadapterPostProcessBackwardHelper.java
+++ org.moflon.tgg.mosl.codeadapter/src/org/moflon/tie/CodeadapterPostProcessBackwardHelper.java
@@ -3,8 +3,6 @@ package org.moflon.tie;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.commons.lang3.StringUtils;
-import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.emf.common.util.BasicEList;
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EClass;
@@ -14,6 +12,7 @@ import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.EReference;
 import org.moflon.tgg.language.Domain;
 import org.moflon.tgg.language.DomainType;
+import org.moflon.tgg.language.TGGObjectVariable;
 import org.moflon.tgg.language.TripleGraphGrammar;
 import org.moflon.tgg.language.csp.AttributeVariable;
 import org.moflon.tgg.language.csp.Literal;
@@ -39,61 +38,62 @@ import org.moflon.tgg.mosl.tgg.Schema;
 import org.moflon.tgg.mosl.tgg.TggFactory;
 import org.moflon.tgg.mosl.tgg.Using;
 import org.moflon.tgg.runtime.CorrespondenceModel;
+
 import SDMLanguage.expressions.ComparisonExpression;
 import SDMLanguage.expressions.Expression;
 import SDMLanguage.patterns.patternExpressions.AttributeValueExpression;
 
 public class CodeadapterPostProcessBackwardHelper {
-	
+
 	private List<String> addedImports;
-	
+
 	public CodeadapterPostProcessBackwardHelper() {
 		addedImports = new ArrayList<String>();
 	}
 
 	public void postProcessBackward(CodeadapterTrafo trafoHelper) {
 		CorrespondenceModel corrModel = trafoHelper.getCorr();
-		
-		for (EObject corr : corrModel.getCorrespondences()) {
-			
-			if(corr instanceof TripleGraphGrammarFileToTripleGraphGrammar)
-				postProcessBackward_TripleGraphGrammarFile((TripleGraphGrammarFileToTripleGraphGrammar)corr);
 
-			if(corr instanceof CorrTypeToEClass)
-				postProcessBackward_CorrType((CorrTypeToEClass)corr);
-			
-			if(corr instanceof RuleToTGGRule)
-				postProcessBackward_Rule((RuleToTGGRule)corr);
-			
-			if(corr instanceof ObjectVariablePatternToTGGObjectVariable)
-				postProcessBackward_ObjectVariablePattern((ObjectVariablePatternToTGGObjectVariable)corr);
-			
-			if(corr instanceof CorrVariablePatternToTGGObjectVariable)
-				postProcessBackward_CorrVariablePattern((CorrVariablePatternToTGGObjectVariable)corr);
-			
-			if(corr instanceof LinkVariablePatternToTGGLinkVariable)
-				postProcessBackward_LinkVariablePattern((LinkVariablePatternToTGGLinkVariable)corr);
-			
-			if(corr instanceof AttributeAssignmentToAttributeAssignment)
-				postProcessBackward_AttributeAssignment((AttributeAssignmentToAttributeAssignment)corr);
-			
-			if(corr instanceof AttributeConstraintToConstraint)
-				postProcessBackward_AttributeConstraint((AttributeConstraintToConstraint)corr);
-			
-			if(corr instanceof ExpressionToExpression)
-				postProcessBackward_Expression((ExpressionToExpression)corr);
-			
-			if(corr instanceof AttrCondToTGGConstraint)
-				postProcessBackward_AttrCond((AttrCondToTGGConstraint)corr);
+		for (EObject corr : corrModel.getCorrespondences()) {
+
+			if (corr instanceof TripleGraphGrammarFileToTripleGraphGrammar)
+				postProcessBackward_TripleGraphGrammarFile((TripleGraphGrammarFileToTripleGraphGrammar) corr);
+
+			if (corr instanceof CorrTypeToEClass)
+				postProcessBackward_CorrType((CorrTypeToEClass) corr);
+
+			if (corr instanceof RuleToTGGRule)
+				postProcessBackward_Rule((RuleToTGGRule) corr);
+
+			if (corr instanceof ObjectVariablePatternToTGGObjectVariable)
+				postProcessBackward_ObjectVariablePattern((ObjectVariablePatternToTGGObjectVariable) corr);
+
+			if (corr instanceof CorrVariablePatternToTGGObjectVariable)
+				postProcessBackward_CorrVariablePattern((CorrVariablePatternToTGGObjectVariable) corr);
+
+			if (corr instanceof LinkVariablePatternToTGGLinkVariable)
+				postProcessBackward_LinkVariablePattern((LinkVariablePatternToTGGLinkVariable) corr);
+
+			if (corr instanceof AttributeAssignmentToAttributeAssignment)
+				postProcessBackward_AttributeAssignment((AttributeAssignmentToAttributeAssignment) corr);
+
+			if (corr instanceof AttributeConstraintToConstraint)
+				postProcessBackward_AttributeConstraint((AttributeConstraintToConstraint) corr);
+
+			if (corr instanceof ExpressionToExpression)
+				postProcessBackward_Expression((ExpressionToExpression) corr);
+
+			if (corr instanceof AttrCondToTGGConstraint)
+				postProcessBackward_AttrCond((AttrCondToTGGConstraint) corr);
 		}
 	}
 
 	private void postProcessBackward_CorrVariablePattern(CorrVariablePatternToTGGObjectVariable corr) {
 		EClass cvType = (EClass) corr.getTarget().getType();
 		Schema schema = ((Rule) corr.getSource().eContainer()).getSchema();
-		
+
 		for (CorrType corrType : schema.getCorrespondenceTypes()) {
-			if(corrType.getName().equals(cvType.getName())) {
+			if (corrType.getName().equals(cvType.getName())) {
 				corr.getSource().setType(corrType);
 			}
 		}
@@ -104,41 +104,51 @@ public class CodeadapterPostProcessBackwardHelper {
 	}
 
 	private void postProcessBackward_ObjectVariablePattern(ObjectVariablePatternToTGGObjectVariable corr) {
-		EClass ovType = (EClass) corr.getTarget().getType();
+		TGGObjectVariable tggOV = corr.getTarget();
+		EClass ovType = (EClass) tggOV.getType();
+		EPackage packageOfOVType = ovType.getEPackage();
 		Schema schema = ((Rule) corr.getSource().eContainer()).getSchema();
+
+		importEPackage(schema, packageOfOVType);
 		
-		importEPackage(schema, ovType);
+		if(tggOV.getDomain().getType().equals(DomainType.SOURCE) && !schema.getSourceTypes().contains(packageOfOVType))
+			schema.getSourceTypes().add(packageOfOVType);
+		
+		if(tggOV.getDomain().getType().equals(DomainType.TARGET) && !schema.getTargetTypes().contains(packageOfOVType))
+			schema.getTargetTypes().add(packageOfOVType);
+		
 		corr.getSource().setType(ovType);
 	}
 
 	private void postProcessBackward_AttrCond(AttrCondToTGGConstraint corr) {
 		AttrCond attrCond = corr.getSource();
 		EList<ParamValue> srcVariables = new BasicEList<ParamValue>();
-		
+
 		for (Variable var : corr.getTarget().getVariables()) {
 			for (ParamValue paramVal : attrCond.getValues()) {
-				if(var instanceof AttributeVariable && paramVal instanceof org.moflon.tgg.mosl.tgg.AttributeVariable){
+				if (var instanceof AttributeVariable && paramVal instanceof org.moflon.tgg.mosl.tgg.AttributeVariable) {
 					org.moflon.tgg.mosl.tgg.AttributeVariable srcAttr = (org.moflon.tgg.mosl.tgg.AttributeVariable) paramVal;
 					AttributeVariable trgAttr = (AttributeVariable) var;
-					
-					if(srcAttr.getAttribute().equals(trgAttr.getAttribute()) && srcAttr.getObjectVar().getName().equals(trgAttr.getObjectVariable()))
+
+					if (srcAttr.getAttribute().equals(trgAttr.getAttribute())
+							&& srcAttr.getObjectVar().getName().equals(trgAttr.getObjectVariable()))
 						srcVariables.add(srcAttr);
 				}
-				if(var instanceof LocalVariable && paramVal instanceof org.moflon.tgg.mosl.tgg.LocalVariable){
+				if (var instanceof LocalVariable && paramVal instanceof org.moflon.tgg.mosl.tgg.LocalVariable) {
 					org.moflon.tgg.mosl.tgg.LocalVariable srcAttr = (org.moflon.tgg.mosl.tgg.LocalVariable) paramVal;
 					LocalVariable trgAttr = (LocalVariable) var;
-					
-					if(srcAttr.getName().equals(trgAttr.getName()))
+
+					if (srcAttr.getName().equals(trgAttr.getName()))
 						srcVariables.add(srcAttr);
 				}
-				if(var instanceof Literal && paramVal instanceof org.moflon.tgg.mosl.tgg.Literal){
+				if (var instanceof Literal && paramVal instanceof org.moflon.tgg.mosl.tgg.Literal) {
 					org.moflon.tgg.mosl.tgg.Literal srcAttr = (org.moflon.tgg.mosl.tgg.Literal) paramVal;
 					Literal trgAttr = (Literal) var;
 					String srcAttrValue = srcAttr.getValue();
 					String trgAttrValue = (String) trgAttr.getValue();
-					
-					if(srcAttrValue.equals(trgAttrValue) || srcAttrValue.equals(trgAttrValue.replace(".", "::"))){
-						if(srcAttrValue.charAt(0) != '"' && srcAttrValue.charAt(0) != '\''){
+
+					if (srcAttrValue.equals(trgAttrValue) || srcAttrValue.equals(trgAttrValue.replace(".", "::"))) {
+						if (srcAttrValue.charAt(0) != '"' && srcAttrValue.charAt(0) != '\'') {
 							srcAttr.setValue(srcAttrValue.replace(".", "::"));
 						}
 						srcVariables.add(srcAttr);
@@ -152,8 +162,9 @@ public class CodeadapterPostProcessBackwardHelper {
 
 	private void postProcessBackward_Expression(ExpressionToExpression corr) {
 		Expression trgExpression = corr.getTarget();
-		if(trgExpression instanceof AttributeValueExpression){
-			((AttributeExpression) corr.getSource()).setAttribute(((AttributeValueExpression) trgExpression).getAttribute());
+		if (trgExpression instanceof AttributeValueExpression) {
+			((AttributeExpression) corr.getSource())
+					.setAttribute(((AttributeValueExpression) trgExpression).getAttribute());
 		}
 	}
 
@@ -170,19 +181,17 @@ public class CodeadapterPostProcessBackwardHelper {
 		CorrType corrType = corr.getSource();
 		EClass corrEClass = corr.getTarget();
 		Schema schema = (Schema) corrType.eContainer();
-		
+
 		setSuperCorrType(corrType, corrEClass, schema);
-		
-		if(corrType.getSuper() == null){
+
+		if (corrType.getSuper() == null) {
 			for (EReference ref : corrEClass.getEAllReferences()) {
 				if (ref.getName().equals("source")) {
 					EClass sourceType = (EClass) ref.getEType();
-					importEPackage(schema, sourceType);
 					corrType.setSource(sourceType);
 				}
 				if (ref.getName().equals("target")) {
 					EClass targetType = (EClass) ref.getEType();
-					importEPackage(schema, targetType);
 					corrType.setTarget(targetType);
 				}
 			}
@@ -192,49 +201,30 @@ public class CodeadapterPostProcessBackwardHelper {
 	private void postProcessBackward_Rule(RuleToTGGRule corr) {
 		Rule rule = corr.getSource();
 		Using using = TggFactory.eINSTANCE.createUsing();
-		
-		using.setImportedNamespace(rule.getSchema().getName() +".*");
+
+		using.setImportedNamespace(rule.getSchema().getName() + ".*");
 		rule.getUsing().add(using);
 	}
 
 	private void postProcessBackward_TripleGraphGrammarFile(TripleGraphGrammarFileToTripleGraphGrammar corr) {
 		TripleGraphGrammar tggRoot = corr.getTarget();
 		Schema schema = corr.getSource().getSchema();
-		
+
 		for (Domain domain : tggRoot.getDomain()) {
-			if(domain.getType() == DomainType.SOURCE){
-				EPackage sourcePackage = importEPackage(schema, domain);
-				schema.getSourceTypes().add(sourcePackage);
+			EPackage domainPackage = domain.getMetamodel().getOutermostPackage();
+			importEPackage(schema, domainPackage);
+			if (domain.getType() == DomainType.SOURCE) {
+				schema.getSourceTypes().add(domainPackage);
 			}
-			if(domain.getType() == DomainType.TARGET){
-				EPackage targetPackage = importEPackage(schema, domain);
-				schema.getTargetTypes().add(targetPackage);
+			if (domain.getType() == DomainType.TARGET) {
+				schema.getTargetTypes().add(domainPackage);
 			}
 		}
-		
+
 	}
 
-	private EPackage importEPackage(Schema schema, Domain domain) {
-		EPackage outermostPackage = domain.getMetamodel().getOutermostPackage();
-		Import packageImport = TggFactory.eINSTANCE.createImport();
-		
-		String importPath = "/"+outermostPackage.getNsPrefix()+"/model/"+StringUtils.capitalize(outermostPackage.getName())+".ecore";
-		
-		if (ResourcesPlugin.getWorkspace().getRoot().findMember(importPath) != null) {
-			importPath = "platform:/resource" + importPath;
-		} else {
-			importPath = outermostPackage.getNsURI();
-		}
-
-		addedImports.add(outermostPackage.getNsURI());
-		packageImport.setName(importPath);
-		schema.getImports().add(packageImport);
-		
-		return outermostPackage;
-	}
-
-	private void importEPackage(Schema schema, EClass targetType) {
-		String ePackageNsURI = targetType.getEPackage().getNsURI();
+	private void importEPackage(Schema schema, EPackage epackage) {
+		String ePackageNsURI = epackage.getNsURI();
 		if (!addedImports.contains(ePackageNsURI)) {
 			addedImports.add(ePackageNsURI);
 			Import packageImport = TggFactory.eINSTANCE.createImport();
@@ -246,7 +236,7 @@ public class CodeadapterPostProcessBackwardHelper {
 	private void setSuperCorrType(CorrType corrType, EClass corrEClass, Schema schema) {
 		for (EClass trgSuperCorrType : corrEClass.getEAllSuperTypes()) {
 			for (EClassifier trgCorrType : corrEClass.getEPackage().getEClassifiers()) {
-				if(trgSuperCorrType.getName().equals(trgCorrType.getName())){
+				if (trgSuperCorrType.getName().equals(trgCorrType.getName())) {
 					for (CorrType srcCorrType : schema.getCorrespondenceTypes()) {
 						if (srcCorrType.getName().equals(trgSuperCorrType.getName())) {
 							corrType.setSuper(srcCorrType);
@@ -256,5 +246,5 @@ public class CodeadapterPostProcessBackwardHelper {
 			}
 		}
 	}
-	
+
 }
