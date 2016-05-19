9998c4fabc2e71fffd140b1d7efd958a1e00da9e
Anthony Anjorin <anthony.anjorin@uni-paderborn.de>
2016-05-19 13:46:53 +0000

    Do not replace CSP library file if it hasn't changed
    Former-commit-id: 4356f4a7ff02d85a9def75e0b1e47a446dd9a922

--- org.moflon.tgg.mosl/src/org/moflon/tgg/mosl/defaults/AttrCondDefLibraryProvider.java
+++ org.moflon.tgg.mosl/src/org/moflon/tgg/mosl/defaults/AttrCondDefLibraryProvider.java
@@ -3,6 +3,11 @@ package org.moflon.tgg.mosl.defaults;
 import static org.moflon.core.utilities.WorkspaceHelper.addAllFoldersAndFile;
 import static org.moflon.core.utilities.WorkspaceHelper.createSubmonitorWith1Tick;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
@@ -11,11 +16,20 @@ import org.eclipse.core.runtime.Path;
 
 public class AttrCondDefLibraryProvider {
 
-	public static void syncAttrCondDefLibrary(IProject project) throws CoreException {
+	public static void syncAttrCondDefLibrary(IProject project) throws CoreException, IOException {
 		String defaultLib = DefaultFilesHelper.generateDefaultAttrCondDefLibrary();
 
 		IPath pathToLib = new Path("src/org/moflon/tgg/mosl/csp/lib/AttrCondDefLibrary.tgg");
-		project.getFile(pathToLib).delete(true, new NullProgressMonitor());
-		addAllFoldersAndFile(project, pathToLib, defaultLib, createSubmonitorWith1Tick(new NullProgressMonitor()));
+		IFile attrLibFile = project.getFile(pathToLib);
+		if(attrLibFile.exists()){
+			File file = new File(attrLibFile.getLocation().toString());
+			String contents = FileUtils.readFileToString(file);
+			if(!contents.equals(defaultLib)){
+				attrLibFile.delete(true, new NullProgressMonitor());
+				addAllFoldersAndFile(project, pathToLib, defaultLib, createSubmonitorWith1Tick(new NullProgressMonitor()));
+			}
+		} else {
+			addAllFoldersAndFile(project, pathToLib, defaultLib, createSubmonitorWith1Tick(new NullProgressMonitor()));
+		}
 	}
 }
