716774a330975333c721554a80bb5b25baa57f73
Gergely Varró <gergely.varro@es.tu-darmstadt.de>
2016-09-01 12:25:10 +0000

    Correcting color enum literals in the visualization projects
    Former-commit-id: d743d2f6f3c4a6396287eaa35ae208f1ff67e705

--- org.moflon.ide.visualization.dot.tgg/src/csp/constraints/SetDefaultColorForTargetDomain.java
+++ org.moflon.ide.visualization.dot.tgg/src/csp/constraints/SetDefaultColorForTargetDomain.java
@@ -10,11 +10,11 @@ public class SetDefaultColorForTargetDomain extends TGGConstraintImpl {
     	
     	switch(bindingStates){
     	case "F":
-    	   var_0.bindToValue(DotColor.MISTYROSE);
+    	   var_0.bindToValue(DotColor.MISTY_ROSE);
     	   setSatisfied(true);
     	   break;
     	case "B":
-    	   setSatisfied(var_0.getValue().equals(DotColor.MISTYROSE));
+    	   setSatisfied(var_0.getValue().equals(DotColor.MISTY_ROSE));
     	   break;
     	default: 
     		throw new UnsupportedOperationException("This case in the constraint has not been implemented yet: " + bindingStates);
