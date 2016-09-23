d4cb023c387b19750b9a0c4033d770fe9f579160
Sascha Edwin Zander <sascha@sezander.de>
2016-09-23 16:45:43 +0200

    added parameters and typing handling post processor

--- org.moflon.ide.visualization.dot.ecore.eclassviz/src/org/moflon/ide/visualization/dot/ecore/eclassviz/EClassDiagramTextProvider.java
+++ org.moflon.ide.visualization.dot.ecore.eclassviz/src/org/moflon/ide/visualization/dot/ecore/eclassviz/EClassDiagramTextProvider.java
@@ -3,6 +3,8 @@ package org.moflon.ide.visualization.dot.ecore.eclassviz;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EPackage;
 import org.moflon.ide.visualisation.dot.language.EMoflonDiagramTextProvider;
+import org.moflon.ide.visualization.dot.ecore.eclassviz.utils.EClassPostProcessor;
+import org.moflon.tgg.runtime.CorrespondenceModel;
 
 public class EClassDiagramTextProvider extends EMoflonDiagramTextProvider {
 
@@ -19,6 +21,11 @@ public class EClassDiagramTextProvider extends EMoflonDiagramTextProvider {
 	@Override
 	protected EPackage getPackage() {
 		return EclassvizPackage.eINSTANCE;
+	}
+	
+	@Override
+	protected void postprocess(CorrespondenceModel corrs) {
+		corrs.getCorrespondences().forEach(corr -> new EClassPostProcessor().postProcess(corr));
 	}
 
 }
---
+++ org.moflon.ide.visualization.dot.ecore.eclassviz/src/org/moflon/ide/visualization/dot/ecore/eclassviz/utils/EClassPostProcessor.java
@@ -0,0 +1,54 @@
+package org.moflon.ide.visualization.dot.ecore.eclassviz.utils;
+
+import org.eclipse.emf.ecore.EClassifier;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.ETypedElement;
+import org.moflon.ide.visualization.dot.ecore.eclassviz.PTypedObjectToETypedElememt;
+import org.moflon.ide.visualization.dot.language.PTypedObject;
+import org.moflon.ide.visualization.dot.language.PVisibilityObject;
+
+public class EClassPostProcessor {
+	public void postProcess(EObject corr){
+		if(corr instanceof PTypedObjectToETypedElememt){
+			handleTyping(PTypedObjectToETypedElememt.class.cast(corr));
+		}
+	}
+	
+	private void handleTyping(PTypedObjectToETypedElememt pte2ete){
+		PTypedObject pTypeElement = pte2ete.getSource();
+		ETypedElement eTypeElement = pte2ete.getTarget();
+		
+		EClassifier eType = eTypeElement.getEType();
+		
+		if(eType == null){
+			pTypeElement.setType("void");
+		}else if(eType != null && eTypeElement.getUpperBound() > 1){
+			pTypeElement.setType("EList<"+eType.getName()+">");
+		}else{
+			pTypeElement.setType(eType.getName());
+		}
+		
+		if (pTypeElement instanceof PVisibilityObject){
+			repairVisibility(PVisibilityObject.class.cast(pTypeElement));
+		}
+	}
+	
+	private void repairVisibility(PVisibilityObject pvo){
+		switch(pvo.getVisibilty()){
+		case PRIVATE_VIEW:
+			pvo.setSign("-");
+			break;
+		case PROTECTED_VIEW:
+			pvo.setSign("#");
+			break;
+		case PACKAGE_ONLY:	
+			pvo.setSign("~");
+			break;
+		case PUBLIC_VIEW:
+			pvo.setSign("+");
+			break;
+		default:
+			break;		
+		}
+	}
+}
