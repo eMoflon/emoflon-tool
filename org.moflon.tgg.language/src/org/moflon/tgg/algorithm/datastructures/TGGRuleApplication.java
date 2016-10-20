c74be21b8fe7b69530da0e24990db15cbc83f088
ErhanLeblebici <erhan.leblebici@es.tu-darmstadt.de>
2016-10-20 18:41:09 +0200

    new protocol type dedicated to a brute force strategy for TGGs

---
+++ org.moflon.tgg.language/src/org/moflon/tgg/algorithm/datastructures/TGGRuleApplication.java
@@ -0,0 +1,60 @@
+package org.moflon.tgg.algorithm.datastructures;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
+import org.eclipse.emf.ecore.EObject;
+
+public class TGGRuleApplication {
+	
+	private HashSet<EObject> createdSrc = new HashSet<>();
+	
+	private HashSet<EObject> createdTrg = new HashSet<>();
+	
+	private HashSet<EObject> createdCorr = new HashSet<>();
+	
+	private HashSet<EObject> contextSrc = new HashSet<>();
+	
+	private HashSet<EObject> contextTrg = new HashSet<>();
+	
+	private HashSet<EObject> contextCorr = new HashSet<>();
+	
+	private Map<String,EObject> nodeMappings = new HashMap<>();
+
+
+	private String name;
+	
+	public TGGRuleApplication(String name){
+		this.name = name;
+	}
+	
+	public HashSet<EObject> getCreatedSrc() {
+		return createdSrc;
+	}
+
+	public HashSet<EObject> getCreatedTrg() {
+		return createdTrg;
+	}
+
+	public HashSet<EObject> getCreatedCorr() {
+		return createdCorr;
+	}
+
+	public HashSet<EObject> getContextSrc() {
+		return contextSrc;
+	}
+
+	public HashSet<EObject> getContextTrg() {
+		return contextTrg;
+	}
+
+	public HashSet<EObject> getContextCorr() {
+		return contextCorr;
+	}
+
+	public Map<String, EObject> getNodeMappings() {
+		return nodeMappings;
+	}
+	
+}
