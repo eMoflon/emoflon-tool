cf14b12884071b31e480d7238bdbde94a957bcce
Sascha Edwin Zander <sascha@sezander.de>
2016-10-28 11:18:30 +0200

    killed unused imports

--- org.moflon.ide.mosl.core/src/org/moflon/ide/mosl/core/scoping/ScopeProviderHelper.java
+++ org.moflon.ide.mosl.core/src/org/moflon/ide/mosl/core/scoping/ScopeProviderHelper.java
@@ -8,12 +8,10 @@ import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 import org.eclipse.emf.common.util.URI;
-import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
-import org.eclipse.xtext.EcoreUtil2;
 import org.eclipse.xtext.scoping.IScope;
 import org.eclipse.xtext.scoping.Scopes;
 
