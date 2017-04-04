9bbbf092f4f2b52b209c2491e147ef7e8f3d306b
Sascha Edwin Zander <sascha@sezander.de>
2017-04-04 11:04:20 +0200

    clean up code

--- org.moflon.gt.mosl.codeadapter/src/org/moflon/gt/mosl/codeadapter/MOSLGTUtil.java
+++ org.moflon.gt.mosl.codeadapter/src/org/moflon/gt/mosl/codeadapter/MOSLGTUtil.java
@@ -1,13 +1,7 @@
 package org.moflon.gt.mosl.codeadapter;
 
-import java.util.Collection;
-
-import org.eclipse.core.resources.IFile;
-import org.eclipse.core.runtime.CoreException;
-import org.gervarro.democles.specification.emf.SpecificationFactory;
 import org.gervarro.democles.specification.emf.constraint.relational.RelationalConstraint;
 import org.gervarro.democles.specification.emf.constraint.relational.RelationalConstraintFactory;
-import org.gervarro.democles.specification.emf.constraint.relational.util.RelationalConstraintAdapterFactory;
 
 public class MOSLGTUtil
 {
@@ -23,32 +17,6 @@ public class MOSLGTUtil
       if (instance == null)
          instance = new MOSLGTUtil();
       return instance;
-   }
-
-   @Deprecated
-   public interface MGTCallbackGetter
-   {
-      Collection<IFile> getMOSLGTFiles() throws CoreException;
-   }
-
-   private MGTCallbackGetter mgtGetter;
-
-   @Deprecated
-   public void setMGTGetter(MGTCallbackGetter mgtGetter)
-   {
-      this.mgtGetter = mgtGetter;
-   }
-
-   @Deprecated
-   public Collection<IFile> getMGTFiles()
-   {
-      try
-      {
-         return this.mgtGetter.getMOSLGTFiles();
-      } catch (CoreException ce)
-      {
-         return null;
-      }
    }
    
    public RelationalConstraint getConstraint(String opValue){
--- org.moflon.ide.core/src/org/moflon/ide/core/runtime/builders/MOSLGTBuilder.java
+++ org.moflon.ide.core/src/org/moflon/ide/core/runtime/builders/MOSLGTBuilder.java
@@ -31,8 +31,6 @@ import org.moflon.core.utilities.LogUtils;
 import org.moflon.core.utilities.WorkspaceHelper;
 import org.moflon.core.utilities.eMoflonEMFUtil;
 import org.moflon.gt.mosl.MOSLGTStandaloneSetupGenerated;
-import org.moflon.gt.mosl.codeadapter.MOSLGTUtil;
-import org.moflon.gt.mosl.codeadapter.MOSLGTUtil.MGTCallbackGetter;
 import org.moflon.ide.core.runtime.CleanVisitor;
 import org.moflon.ide.core.runtime.MoflonProjectCreator;
 import org.moflon.util.plugins.manifest.ExportedPackagesInManifestUpdater;
@@ -129,20 +127,7 @@ public class MOSLGTBuilder extends AbstractVisitorBuilder
       initializeResourceSet();
 
       final MoflonCodeGenerator codeGenerationTask = new MoflonCodeGenerator(WorkspaceHelper.getDefaultEcoreFile(getProject()), resourceSet);
-      // collectMOSLGTFiles();
-     
-      MOSLGTUtil.getInstance().setMGTGetter(new MGTCallbackGetter() {
-
-         @Override
-         public Collection<IFile> getMOSLGTFiles() throws CoreException
-         {
-            return collectMOSLGTFiles();
-         }
-      });
-
       final IStatus status = codeGenerationTask.run(subMon.split(7));
-
-      // loadMGTFiles(monitor);
       handleErrorsAndWarnings(status);
       subMon.worked(2);
 
@@ -158,31 +143,6 @@ public class MOSLGTBuilder extends AbstractVisitorBuilder
       this.resourceSet.addLoadOption(XtextResource.OPTION_RESOLVE_ALL, Boolean.TRUE);
       eMoflonEMFUtil.installCrossReferencers(this.resourceSet);
    }
-
-   //   @Deprecated
-   //   private Collection<IFile> collectMOSLGTFiles() throws CoreException
-   //   {
-   //      final Collection<IFile> moslGTFiles = new ArrayList<>();
-   //      getProject().accept(new IResourceVisitor() {
-   //
-   //         @Override
-   //         public boolean visit(IResource resource) throws CoreException
-   //         {
-   //            if (isMOSLGTFile(resource))
-   //               moslGTFiles.add(IFile.class.cast(resource));
-   //            return true;
-   //         }
-   //
-   //         private boolean isMOSLGTFile(IResource resource)
-   //         {
-   //            return resource != null && resource.exists() && resource instanceof IFile
-   //                  && Arrays.asList(resource.getFullPath().segments()).stream().anyMatch(s -> "src".equals(s))
-   //                  && resource.getName().endsWith("." + WorkspaceHelper.MOSL_GT_EXTENSION);
-   //         }
-   //      });
-   //
-   //      return moslGTFiles;
-   //   }
 
    private IProject updateProjectStructure(final IProgressMonitor monitor) throws CoreException
    {
