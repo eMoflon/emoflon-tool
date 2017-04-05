d73a44298d5de2662cb39f40ce48a61f5561166e
Roland Kluge <roland.kluge@es.tu-darmstadt.de>
2017-04-05 10:11:32 +0200

    Add docu and refactoring

--- org.moflon.ide.core/src/org/moflon/ide/core/runtime/builders/MOSLGTBuilder.java
+++ org.moflon.ide.core/src/org/moflon/ide/core/runtime/builders/MOSLGTBuilder.java
@@ -134,10 +134,15 @@ public class MOSLGTBuilder extends AbstractVisitorBuilder
       postprocessAfterCodeGeneration(codeGenerationTask.getGenModel(), subMon.split(1));
    }
 
+   /**
+    * Prepare an {@link ResourceSet} that is suitable for a MOSL-GT-based build process
+    * 
+    * @return the initialized resource set
+    */
    public static XtextResourceSet initializeResourceSet()
    {
       // See also: https://wiki.eclipse.org/Xtext/FAQ#How_do_I_load_my_model_in_a_standalone_Java_application.C2.A0.3F
-      Injector injector = new MOSLGTStandaloneSetupGenerated().createInjectorAndDoEMFRegistration();
+      final Injector injector = new MOSLGTStandaloneSetupGenerated().createInjectorAndDoEMFRegistration();
       final XtextResourceSet resourceSet = injector.getInstance(XtextResourceSet.class);
       // eMoflonEMFUtil.initializeDefault(this.resourceSet);
       resourceSet.addLoadOption(XtextResource.OPTION_RESOLVE_ALL, Boolean.TRUE);
