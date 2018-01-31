46dd96a9947eb4d04506456b73c1d917fd678f29
Roland Kluge <roland.kluge@es.tu-darmstadt.de>
2018-01-31 09:37:42 +0100

    Docu

--- org.moflon.ide.core/src/org/moflon/ide/core/MoslTggConstants.java
+++ org.moflon.ide.core/src/org/moflon/ide/core/MoslTggConstants.java
@@ -1,11 +1,15 @@
 package org.moflon.ide.core;
 
+/**
+ * Constants related to the MOSL TGG specification language
+ *
+ * @author Roland Kluge - Initial implementation
+ *
+ */
 public class MoslTggConstants
 {
-
    public static final String MOSL_TGG_EXTENSION = "tgg";
    public static final String TGG_FILE_EXTENSION = ".tgg.xmi";
    public static final String PRE_TGG_FILE_EXTENSION = ".pre.tgg.xmi";
    public static final String PRE_ECORE_FILE_EXTENSION = ".pre.ecore";
-
 }
