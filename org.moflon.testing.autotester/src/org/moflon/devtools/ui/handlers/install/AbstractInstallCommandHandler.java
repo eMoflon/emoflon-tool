00e60c6632ccabc78d957b654c7d573e083e0496
Roland Kluge <roland.kluge@es.tu-darmstadt.de>
2018-02-01 16:24:16 +0100

    Remove code duplication

--- org.moflon.testing.autotester/src/org/moflon/devtools/ui/handlers/install/AbstractInstallCommandHandler.java
+++ org.moflon.testing.autotester/src/org/moflon/devtools/ui/handlers/install/AbstractInstallCommandHandler.java
@@ -2,7 +2,7 @@ package org.moflon.devtools.ui.handlers.install;
 
 import org.eclipse.core.commands.ExecutionEvent;
 import org.moflon.autotest.core.WorkspaceInstaller;
-import org.moflon.devtools.ui.handlers.AbstractCommandHandler;
+import org.moflon.core.ui.AbstractCommandHandler;
 
 /**
  * Parent class for all command handlers that checkout workspaces.
