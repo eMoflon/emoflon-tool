fe42d744e009ada1b5a2d6b63d36f35d0a7a897c
Roland Kluge <roland.kluge@es.tu-darmstadt.de>
2018-03-23 09:36:33 +0100

    Refactoring

--- org.moflon.ide.ui/src/org/moflon/ide/ui/admin/handlers/AbstractValidateHandler.java
+++ org.moflon.ide.ui/src/org/moflon/ide/ui/admin/handlers/AbstractValidateHandler.java
@@ -10,7 +10,6 @@ import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.NullProgressMonitor;
@@ -18,10 +17,10 @@ import org.eclipse.core.runtime.OperationCanceledException;
 import org.eclipse.core.runtime.SubMonitor;
 import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.core.runtime.jobs.JobGroup;
-import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.jface.viewers.TreeSelection;
-import org.eclipse.ui.IWorkingSet;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.handlers.HandlerUtil;
 import org.gervarro.eclipse.task.ITask;
@@ -33,15 +32,32 @@ import org.moflon.ide.ui.preferences.EMoflonPreferenceInitializer;
 /**
  * Implementation base for validation with and without dumping the intermediate
  * models
- * 
+ *
  * @author Roland Kluge - Initial implementation
  *
  */
 public abstract class AbstractValidateHandler extends AbstractCommandHandler {
 
 	/**
+	 * Invokes {@link #validateProjects(Collection, IProgressMonitor)} for all
+	 * projects than can be found in the selection of the given event
+	 */
+	@Override
+	public Object execute(final ExecutionEvent event) throws ExecutionException {
+		final Collection<IFile> files = this.extractSelectedFiles(event);
+		final Collection<IProject> projects = this.extractSelectedProjects(event);
+		try {
+			this.validateFiles(files, new NullProgressMonitor());
+			this.validateProjects(projects, new NullProgressMonitor());
+		} catch (CoreException e) {
+			throw new ExecutionException(e.getMessage(), e);
+		}
+		return AbstractCommandHandler.DEFAULT_HANDLER_RESULT;
+	}
+
+	/**
 	 * Validates the given Ecore file, is known to exist
-	 * 
+	 *
 	 * @param ecoreFile
 	 *            the Ecore file
 	 * @param monitor
@@ -52,42 +68,117 @@ public abstract class AbstractValidateHandler extends AbstractCommandHandler {
 	protected abstract void validateFile(final IFile ecoreFile, final IProgressMonitor monitor) throws CoreException;
 
 	/**
-	 * Invokes {@link #validateProjects(Collection, IProgressMonitor)} for all
-	 * projects than can be found in the selection of the given event
+	 * Invokes {@link #validateFile(IFile, IProgressMonitor)} for each file in the given list
+	 * @param files the files to validate
+	 * @param monitor the progress monitor
+	 * @throws CoreException if validation crashes
 	 */
-	@Override
-	public Object execute(final ExecutionEvent event) throws ExecutionException {
-		final ISelection selection = HandlerUtil.getCurrentSelectionChecked(event);
-		try {
-			if (selection instanceof TreeSelection) {
-				final TreeSelection treeSelection = (TreeSelection) selection;
-				for (final Iterator<?> selectionIterator = treeSelection.iterator(); selectionIterator.hasNext();) {
-					final Object element = selectionIterator.next();
-					if (element instanceof IFile) {
-						validateFile((IFile) element, new NullProgressMonitor());
-					} else if (element instanceof IJavaProject) {
-						validateProject(((IJavaProject) element).getProject(), new NullProgressMonitor());
-					} else if (element instanceof IWorkingSet) {
-						final List<IProject> projects = new ArrayList<>();
-						final IWorkingSet workingSet = (IWorkingSet) element;
-						for (final IAdaptable elementInWorkingSet : workingSet.getElements()) {
-							if (elementInWorkingSet instanceof IProject) {
-								projects.add((IProject) elementInWorkingSet);
-							}
-						}
-						validateProjects(projects, new NullProgressMonitor());
-					}
-				}
-			}
-		} catch (final CoreException e) {
-			throw new ExecutionException(e.getMessage());
+	protected final void validateFiles(final Collection<IFile> files, final IProgressMonitor monitor) throws CoreException {
+		final SubMonitor subMon = SubMonitor.convert(monitor, files.size());
+		for (final IFile file : files) {
+			validateFile(file, subMon.split(1));
 		}
-		return null;
+	}
+	/**
+	 * Runs the given validation task using {@link #runWithTimeout(Job)}
+	 *
+	 * @param validationTask
+	 *            the validation task to run
+	 */
+	protected void runValidation(final ITask validationTask) {
+		final Job job = new ValidationTaskJob(validationTask);
+
+		configureBackOrForeground(job);
+
+		runWithTimeout(job);
+	}
+
+	private Collection<IProject> extractSelectedProjects(ExecutionEvent event) throws ExecutionException {
+		final ISelection selection = HandlerUtil.getCurrentSelectionChecked(event);
+		if (selection instanceof StructuredSelection) {
+			return getProjectsFromSelection((IStructuredSelection)selection);
+		} else {
+			return new ArrayList<>();
+		}
+	}
+
+	private List<IFile> extractSelectedFiles(ExecutionEvent event) throws ExecutionException {
+		final List<IFile> files = new ArrayList<>();
+		final ISelection selection = HandlerUtil.getCurrentSelectionChecked(event);
+		final TreeSelection treeSelection = (TreeSelection) selection;
+		for (final Iterator<?> selectionIterator = treeSelection.iterator(); selectionIterator.hasNext();) {
+			final Object element = selectionIterator.next();
+			if (element instanceof IFile) {
+				files.add((IFile) element);
+			}
+		}
+		return files;
+	}
+
+	/**
+	 * Runs the given {@link Job} and limits its execution time via the validation
+	 * timeout configured via the {@link EMoflonPreferenceInitializer}
+	 *
+	 * @param job
+	 */
+	private void runWithTimeout(final Job job) {
+		final long validationTimeoutMillis = EMoflonPreferenceInitializer.getValidationTimeoutMillis();
+		final JobGroup jobGroup = new JobGroup("Validation job group", 1, 1);
+		job.setJobGroup(jobGroup);
+		job.schedule();
+		try {
+			final boolean completed = jobGroup.join(validationTimeoutMillis, new NullProgressMonitor());
+			if (!completed) {
+				stopJob(job);
+				logger.error(getValidationTimeoutMessage(validationTimeoutMillis));
+				throw new OperationCanceledException(getValidationTimeoutMessage(validationTimeoutMillis));
+			}
+		} catch (final InterruptedException e) {
+			throw new OperationCanceledException(getValidationTimeoutMessage(validationTimeoutMillis));
+		}
+	}
+
+	/**
+	 * Prepares the job to run in background if required
+	 *
+	 * @param job
+	 *            the job to configure
+	 */
+	private static void configureBackOrForeground(final Job job) {
+		final boolean runInBackground = PlatformUI.getPreferenceStore().getBoolean("RUN_IN_BACKGROUND");
+		if (!runInBackground) {
+			PlatformUI.getWorkbench().getProgressService().showInDialog(null, job);
+		}
+	}
+
+	/**
+	 * Formats the error message for the timeout message
+	 *
+	 * @param validationTimeoutMillis
+	 *            the timeout in millis
+	 * @return the formatted message
+	 */
+	private static String getValidationTimeoutMessage(final long validationTimeoutMillis) {
+		final String validationTimeoutMessage = String.format(
+				"Validation took longer than %ds. This could(!) mean that some of your patterns have no valid search plan. You may increase the timeout value using the eMoflon property page",
+				validationTimeoutMillis / 1000);
+		return validationTimeoutMessage;
+	}
+
+	/**
+	 * Stops the thread running the given {@link Job}
+	 *
+	 * @param job
+	 *            the job to stop
+	 */
+	@SuppressWarnings("deprecation")
+	private void stopJob(final Job job) {
+		job.getThread().stop();
 	}
 
 	/**
 	 * Invokes {@link #validateProject(IProject, IProgressMonitor)} for each project
-	 * 
+	 *
 	 * @param projects
 	 * @param monitor
 	 * @throws CoreException
@@ -104,7 +195,7 @@ public abstract class AbstractValidateHandler extends AbstractCommandHandler {
 	/**
 	 * Invokes {@link #validateFile(IFile, IProgressMonitor)} for each project that
 	 * is a Moflon project
-	 * 
+	 *
 	 * @param project
 	 * @param monitor
 	 * @throws CoreException
@@ -116,36 +207,23 @@ public abstract class AbstractValidateHandler extends AbstractCommandHandler {
 		}
 	}
 
-	@SuppressWarnings("deprecation")
-	protected void runValidation(final ITask validationTask) {
-		final long validationTimeoutMillis = EMoflonPreferenceInitializer.getValidationTimeoutMillis();
-		final String validationTimeoutMessage = "Validation took longer than " + validationTimeoutMillis / 1000
-				+ "s. This could(!) mean that some of your patterns have no valid search plan. You may increase the timeout value using the eMoflon property page";
-	
-		final Job job = new Job(validationTask.getTaskName()) {
-			@Override
-			protected IStatus run(final IProgressMonitor monitor) {
-				return validationTask.run(monitor);
-			}
-		};
-	
-		final boolean runInBackground = PlatformUI.getPreferenceStore().getBoolean("RUN_IN_BACKGROUND");
-		if (!runInBackground) {
-			PlatformUI.getWorkbench().getProgressService().showInDialog(null, job);
+	/**
+	 * This {@link Job} runs the configured task during its
+	 * {@link #run(IProgressMonitor)} method
+	 *
+	 * @author Roland Kluge - Initial implementation
+	 */
+	private static final class ValidationTaskJob extends Job {
+		private final ITask validationTask;
+
+		private ValidationTaskJob(final ITask validationTask) {
+			super(validationTask.getTaskName());
+			this.validationTask = validationTask;
 		}
-	
-		final JobGroup jobGroup = new JobGroup("Validation job group", 1, 1);
-		job.setJobGroup(jobGroup);
-		job.schedule();
-		try {
-			boolean completed = jobGroup.join(validationTimeoutMillis, new NullProgressMonitor());
-			if (!completed) {
-				job.getThread().stop();
-				logger.error(validationTimeoutMessage);
-				throw new OperationCanceledException(validationTimeoutMessage);
-			}
-		} catch (InterruptedException e) {
-			throw new OperationCanceledException(validationTimeoutMessage);
+
+		@Override
+		protected IStatus run(final IProgressMonitor monitor) {
+			return validationTask.run(monitor);
 		}
 	}
 }
