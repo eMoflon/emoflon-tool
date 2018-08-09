ee4f06fbc54a50fc04ad0f0bf70dce951bc85d87
Roland Kluge <roland.kluge@es.tu-darmstadt.de>
2018-08-09 16:10:46 +0200

    Create workaround for saving models with non-Serializable objects
    The workaround searches for Constants with non-Serializeable values and
    temporarily removes these values from the model.
    
    Solves https://github.com/eMoflon/emoflon-tool/issues/340

--- org.moflon.sdm.compiler.democles/src/org/moflon/compiler/sdm/democles/eclipse/DemoclesValidationUtils.java
+++ org.moflon.sdm.compiler.democles/src/org/moflon/compiler/sdm/democles/eclipse/DemoclesValidationUtils.java
@@ -1,8 +1,10 @@
 package org.moflon.compiler.sdm.democles.eclipse;
 
 import java.io.IOException;
+import java.io.Serializable;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.log4j.Logger;
 import org.eclipse.core.resources.IFolder;
@@ -10,6 +12,7 @@ import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IResourceVisitor;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.emf.common.util.TreeIterator;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EOperation;
@@ -17,6 +20,7 @@ import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.URIConverter;
 import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.gervarro.democles.specification.emf.Constant;
 import org.moflon.compiler.sdm.democles.DemoclesMethodBodyHandler;
 import org.moflon.core.utilities.LogUtils;
 import org.moflon.core.utilities.UtilityClassNotInstantiableException;
@@ -25,7 +29,7 @@ import org.moflon.core.utilities.eMoflonEMFUtil;
 
 /**
  * Useful utilities for the democles validation process
- * 
+ *
  * @author Roland Kluge - Original implementation
  */
 public final class DemoclesValidationUtils {
@@ -38,7 +42,7 @@ public final class DemoclesValidationUtils {
 	/**
 	 * This method persists the intermediate control flow structures that are
 	 * produced during the Democles validation process
-	 * 
+	 *
 	 * @param ePackage
 	 */
 	public static void saveAllIntermediateModels(final EPackage ePackage) {
@@ -80,9 +84,9 @@ public final class DemoclesValidationUtils {
 
 	/**
 	 * Persists the given resource using its URL plus the suffix .xmi.
-	 * 
+	 *
 	 * The resource will is not changed during this method.
-	 * 
+	 *
 	 * @param adapterResource
 	 *            the resource to save
 	 */
@@ -96,25 +100,69 @@ public final class DemoclesValidationUtils {
 			} else {
 				inWorkspaceUri = oldUri;
 			}
+			// Save with old URI to allow for navigation between models
+			adapterResource.setURI(inWorkspaceUri);
+
 			try {
-				// Save with old URI to allow for navigation between models
-				adapterResource.setURI(inWorkspaceUri);
-				HashMap<Object, Object> saveOptions = new HashMap<>();
+
+				final Map<Constant, Object> temporarilyClearedConstants = removeAndSaveAllNonSerializeableValues(
+						adapterResource);
+
+				final HashMap<Object, Object> saveOptions = new HashMap<>();
 				saveOptions.put(Resource.OPTION_LINE_DELIMITER, WorkspaceHelper.DEFAULT_RESOURCE_LINE_DELIMITER);
 				adapterResource.save(saveOptions);
 				URIConverter.URI_MAP.put(oldUri, inWorkspaceUri);
+
+				restoreNonSerializeableValues(temporarilyClearedConstants);
 			} catch (IOException e) {
 				LogUtils.error(logger, e);
 			} finally {
 				adapterResource.setURI(oldUri);
+				URIConverter.URI_MAP.remove(oldUri);
 			}
 		}
 	}
 
 	/**
+	 * Prepares the given resource for serialization via {@link Resource#save(Map)}
+	 *
+	 * Iterates through the given resource and sets the value of all
+	 * {@link Constant} objects to <code>null</code> if the value is not
+	 * {@link Serializable}.
+	 *
+	 * The returned map saves the original value assignments of the modified {@link Constant} objects
+	 *
+	 * @param adapterResource the resource to update
+	 * @return the lookup table of all {@link Constant} objects whose value was set to <code>null</code>
+	 */
+	private static Map<Constant, Object> removeAndSaveAllNonSerializeableValues(AdapterResource adapterResource) {
+		final Map<Constant, Object> temporarilyClearedConstants = new HashMap<>();
+		final TreeIterator<Object> allContentsIterator = EcoreUtil.getAllContents(adapterResource.getContents());
+		while (allContentsIterator.hasNext()) {
+			final Object next = allContentsIterator.next();
+			if (next instanceof Constant) {
+				final Constant constant = (Constant) next;
+				if (!(constant.getValue() instanceof Serializable)) {
+					temporarilyClearedConstants.put(constant, constant.getValue());
+					constant.setValue(null);
+				}
+			}
+		}
+		return temporarilyClearedConstants;
+	}
+
+	/**
+	 * For each stored {@link Constant}-Object mapping, this method sets the value of the constant to be the respective value.
+	 * @param temporarilyClearedConstants the lookup from {@link Constant} to objects
+	 */
+	private static void restoreNonSerializeableValues(final Map<Constant, Object> temporarilyClearedConstants) {
+		temporarilyClearedConstants.forEach((constant, value) -> constant.setValue(value));
+	}
+
+	/**
 	 * Deletes all intermediate models of the control flow generation process, which
 	 * are generated, e.g., by #saveAllIntermediateModels.
-	 * 
+	 *
 	 * @param modelFolder
 	 *            only files contained in this folder are scanned
 	 */
